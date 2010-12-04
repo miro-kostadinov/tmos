4cc1b945a0aebaaa0a2feba5d3046013b3e628c9
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2010-12-04 21:57:04 +0000

    1st commit

---
+++ trunk/doxygen/mainpage.h
@@ -0,0 +1,25 @@
+/**
+ * @file mainpage.h
+ * @brief file used for doxygen
+ *
+ *  Created on: 16.11.2010
+ *      Author: Miroslav Kostadinov
+ *
+ *
+ *
+ *
+ * @mainpage TMOS operating system
+ *
+ * @section project_sec Introduction
+ *
+ *   TMOS introduction here...
+ *
+ *   The project structure
+ *
+ *   @subsection make_ssec make system
+ *   @subsection cores_ssec cores
+ *   @subsection drivers_ssec drivers
+ *   @subsection services_ssec services
+ *
+ */
+
