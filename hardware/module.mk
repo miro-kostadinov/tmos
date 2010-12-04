34f7af638c0e78ffeb1b5632fa0f66e7e76be9f2
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2010-12-04 21:57:31 +0000

    1st commit

---
+++ hardware/module.mk
@@ -0,0 +1,33 @@
+##########################################################################
+#
+#	Make include for hardware
+#	
+#   Platform: MPOS rev.3.0	
+#
+#
+##########################################################################
+
+# local sources
+local_as_src-y	:=
+ 
+local_c_src-y 	:= 
+
+local_cpp_src-y	:=   
+
+
+#updating global variables
+as_sources 	+= $(call changepath,$(local_as_src-y))
+c_sources  	+= $(call changepath,$(local_c_src-y))
+cpp_sources += $(call changepath,$(local_cpp_src-y))
+
+
+#add current directory to include path
+inc_dirs += $(subdirectory) 
+
+#submodules
+local_modules-y := $(CFG_FAMILY) 
+
+sub_modules := $(call changepath,$(local_modules-y))
+modules += $(sub_modules)
+
+include $(addsuffix /module.mk,$(sub_modules))
