f36e220cb8c66df38f9bf11f9e61ea22362716c2
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2010-12-06 12:19:37 +0000

    doxygen

--- trunk/tmos/doxygen/mainpage.h
+++ trunk/tmos/doxygen/mainpage.h
@@ -52,7 +52,25 @@
  * default folder (tmos/).
  *
  *
+ *@page nam_conv Naming conventions
  *
+ * @par For C functions
+ *
+ * It is recommended to use names in the form "aaa_bbbb_cccc()" where "aaa" is
+ * optional function type or caller area.
+ * For example all "tsk_xxx()" functions can be called from tasks but cannot be
+ * called from service mode. While all "svc_xxx()" can be called from service
+ * mode only.
+ *
+ * When using multiple words for the function name they should be glued with "_",
+ * for example "string_cmpare()". The word ordering sometimes can be wrong from
+ * the English grammar point of view. For example "green house" means that the
+ * object is a house which is supposed to be green. So if we have a function
+ * that returns such object one can expect that it should be called
+ * "green_house()".
+ * However, if we have a library for "house" related functions they all should
+ * be named as "house_xxx()", so "house_green()" is preferred rather than
+ * "green_house()".
  *
  */
 
