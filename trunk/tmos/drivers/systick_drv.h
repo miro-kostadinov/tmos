444bc7739342825a839cf4430372622db4875d26
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2010-12-07 11:42:21 +0000

    common SYSTICK driver for all cortex-m3 CPUs

---
+++ trunk/tmos/drivers/systick_drv.h
@@ -0,0 +1,56 @@
+/**
+ * @ingroup	 drivers
+ * @defgroup DRV_SYSTICK SYSTICK Driver
+ * Interface for SYSTICK driver.
+ * @{
+ *
+ * This driver is used from the TMOS kernel for task schedule & timeout.
+ *
+ * @note The Systick driver is special driver. It must be declared
+ * (in drivers.cpp) but the installation is done internally (not in DRV_TABLE).
+ *
+ * The driver cannot be used with handles, so it does not need DSR().
+ *
+ *
+ * @note Systick ISR is written in assembler (services.S)
+ *
+ * @file     drivers/systick_drv.h
+ * @ingroup	 DRV_SYSTICK
+ * @brief    SYSTICK driver header
+ *
+ * @date     29. November 2010
+ * @author	 Miroslav Kostadinov
+ * @}
+*/
+#ifndef CLOCK_DRV_H_
+#define CLOCK_DRV_H_
+
+#include <tmos.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+struct SYSTICK_DRIVER_INFO
+{
+	DRIVER_INFO_Type info;				//!< standard driver info
+	unsigned int OS_QUANTUM_PERIOD;		//!< quantum [ms] used from the kernel
+};
+
+/** SYSTICK Driver Info */
+typedef const SYSTICK_DRIVER_INFO* SYSTICK_INFO;
+
+void SYSTICK_DCR(SYSTICK_INFO drv_info, unsigned int reason, void* param);
+
+/**
+ * Systick ISR - written in assembler (services.S)
+ * @param driver
+ */
+void isr_ClockDriver(SYSTICK_INFO driver);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* CLOCK_DRV_H_ */
