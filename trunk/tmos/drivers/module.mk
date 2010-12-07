28e723714d41621268da26b439ac14b906a4bf41
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2010-12-07 16:50:58 +0000

    exporting headers

--- trunk/tmos/drivers/module.mk
+++ trunk/tmos/drivers/module.mk
@@ -9,19 +9,21 @@
 
 # local sources
 local_as_src-y	:=
- 
 local_c_src-y 	:= 
-
 local_cpp_src-y	:=   
+local_h_src-y 	:=
 
 ifeq ($(CFG_CORE), cortex-m3)
 local_cpp_src-y	+= systick_drv.cpp
+local_h_src-y 	+= systick_drv.h
 endif
+
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
 c_sources  	+= $(call changepath,$(local_c_src-y))
 cpp_sources += $(call changepath,$(local_cpp_src-y))
+h_sources  	+= $(call changepath,$(local_h_src-y))
 
 #add current directory to include path
 inc_dirs += $(subdirectory) 
