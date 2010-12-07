7b267a7fc716e8ae391b2e54420a86325d5df701
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2010-12-07 11:43:15 +0000

    common SYSTICK driver for all cortex-m3 CPUs

--- tmos/drivers/at91/cortex-m3/module.mk
+++ tmos/drivers/at91/cortex-m3/module.mk
@@ -13,7 +13,7 @@ local_as_src-y	:=
 local_c_src-y 	:= 
 
 local_cpp_src-y	:= supc_drv.cpp rstc_drv.cpp rtc_drv.cpp rtt_drv.cpp pmc_drv.cpp
-local_cpp_src-y	+= eefc_drv.cpp wdt_drv.cpp systick_drv.cpp
+local_cpp_src-y	+= eefc_drv.cpp wdt_drv.cpp 
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
