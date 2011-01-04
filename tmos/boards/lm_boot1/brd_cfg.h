fb65a327182f8e3cb817365adc1a1eac689b3502
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-01-04 14:40:35 +0000

    simple bootstrap config for LM3S

---
+++ tmos/boards/lm_boot1/brd_cfg.h
@@ -0,0 +1,26 @@
+/*
+ * tmos_cfg.h
+ *
+ *  Created on: 10.11.2010
+ *      Author: Miroslav Kostadinov
+ */
+
+
+//* TMOS related configurations
+
+#ifndef BRD_CFG_H_
+#define BRD_CFG_H_
+
+#define HW_VER_11			1
+
+
+#define USE_TMOS_STRING		1
+#define USE_CSTRING			0
+#define USE_TMOS_STDLIB		0
+#define USE_TIME			0
+#define USE_TMOS_STDIO		1
+#define USE_CRC32			1
+#define USE_CACHE			0
+#define USE_SHA_1			0
+
+#endif /* BRD_CFG_H_ */
---
+++ tmos/boards/lm_boot1/module.mk
@@ -0,0 +1,98 @@
+#-------------------------------------------------------------------------------
+#
+#	Config file for lm_boot1
+#	
+#   www.thumbos.com
+#
+#-------------------------------------------------------------------------------
+
+OUT_NAME	?= lm_boot1
+OUT_DIR		?= out/$(OUT_NAME)/
+
+#  optimisation level  can be [0, 1, 2, 3, s].
+OPT			?= -O0
+
+# Build library/binary/hex. can be [y, n]
+BUILD_LIB	?= y
+BUILD_BIN	?= n
+BUILD_HEX	?= n
+
+# Hardware selection
+CFG_CORE 	?= cortex-m3
+CFG_FAMILY	?= lm3s
+CFG_MCU 	?= LM3S5B91_C1
+
+
+# Debugging format.
+DEBUG 		?= dwarf-2
+
+
+#===============	Project settings		===============#
+
+# Select Target CPU
+
+
+# Select Target board
+#BOARD_TYPE  = HW_VER_10
+BOARD_TYPE  ?= HW_VER_11
+
+
+#===============	TMOS settings		===============#
+
+# memcmp() memset() memclr() memcpy() memmove() strcpy() strncpy() strlen()
+# strchr() strrchr() strpbrk() islower() isupper() strcmp() strncmp() strcasecmp()
+# strncasecmp() toupper() tolower() XNUM_TO_DIGIT()
+USE_TMOS_STRING	?=y
+
+#CSTRING class
+USE_CSTRING		?=n
+
+# tmos stdlib -> sys_malloc() sys_realloc() svc_free()
+USE_TMOS_STDLIB	?=n
+
+# class CTime
+USE_TIME		?=n
+
+# sprintf() scanf() 
+USE_TMOS_STDIO	?=y
+
+#CalculateCRC32()
+USE_CRC32		?=y
+
+# class CCache
+USE_CACHE		?=n
+
+# SHA-1 class
+USE_SHA_1		?=n
+
+
+
+##########################################################################
+
+# local sources
+local_as_src-y	:=
+local_c_src-y 	:=
+local_cpp_src-y	:=
+local_h_src-y 	:=
+
+local_c_src-y 	+= swi_tab.c
+ 
+local_h_src-y 	+= brd_cfg.h brd_cpp.h swi_tab.h trace.h
+
+#updating global variables
+as_sources 	+= $(call changepath,$(local_as_src-y))
+c_sources  	+= $(call changepath,$(local_c_src-y))
+cpp_sources += $(call changepath,$(local_cpp_src-y))
+h_sources  	+= $(call changepath,$(local_h_src-y))
+
+#add current directory to include path
+inc_dirs += $(subdirectory) 
+
+#submodules
+local_modules-y := 
+
+sub_modules := $(call changepath,$(local_modules-y))
+modules += $(sub_modules)
+
+include $(addsuffix /module.mk,$(sub_modules))
+
