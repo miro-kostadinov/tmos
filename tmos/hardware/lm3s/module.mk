8522811bc75aba513f8253fc03676be774f6d97b
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-01-13 10:37:11 +0000

    lm3s flash controller

--- tmos/hardware/lm3s/module.mk
+++ tmos/hardware/lm3s/module.mk
@@ -14,12 +14,12 @@ local_cpp_src-y	:=
 local_h_src-y 	:=
 
 local_cpp_src-y	+= adc_lm3s.cpp gpio_lm3s.cpp ssi_lm3s.cpp sysctl_lm3s.cpp 
-local_cpp_src-y	+= uart_lm3s.cpp timer_lm3s.cpp
+local_cpp_src-y	+= uart_lm3s.cpp timer_lm3s.cpp flash_lm3s.cpp
 
 local_h_src-y 	+= fam_cpp.h fam_inc.h memmap_inc.h sysctl_lm3s.h uart_lm3s.h 
 local_h_src-y 	+= gpio_lm3s.h udma_lm3s.h ssi_lm3s.h adc_lm3s.h gpio_inc.h 
 local_h_src-y 	+= ssi_inc.h uart_inc.h sysctl_inc.h types_lm3s.h timer_inc.h
-local_h_src-y 	+= timer_lm3s.h
+local_h_src-y 	+= timer_lm3s.h flash_lm3s.h
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
