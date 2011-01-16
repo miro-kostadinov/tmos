f22e4bc71a3a3730d235e65aec5e911dd12b3549
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-01-16 14:05:42 +0000

    AES TMOS assembler implementation

--- tmos/services/module.mk
+++ tmos/services/module.mk
@@ -13,8 +13,12 @@ local_c_src-y 	:=
 local_cpp_src-y	:=   
 local_h_src-y 	:=
 
+local_as_src-$(USE_AES_TMOS)+= aes_tmos.S
+
 local_cpp_src-$(USE_SHA_1) 	+= sha_1.cpp
+
 local_h_src-$(USE_SHA_1) 	+= sha_1.h
+local_h_src-$(USE_AES_TMOS)	+= aes_tmos.h
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
@@ -25,6 +29,8 @@ h_sources  	+= $(call changepath,$(local_h_src-y))
 
 #submodules
 local_modules-y := 
+
+local_modules-$(USE_POLARSSL)	+= PolarSSL
 
 sub_modules := $(call changepath,$(local_modules-y))
 modules += $(sub_modules)
