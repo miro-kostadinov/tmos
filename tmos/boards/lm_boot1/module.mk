99b18afe0f8e8962fe2c5e20e6bf884ac05a7853
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-01-16 14:10:25 +0000

    board release

--- tmos/boards/lm_boot1/module.mk
+++ tmos/boards/lm_boot1/module.mk
@@ -10,7 +10,7 @@ OUT_NAME	?= lm_boot1
 OUT_DIR		?= out/$(OUT_NAME)/
 
 #  optimisation level  can be [0, 1, 2, 3, s].
-OPT			?= -O0
+OPT			?= -Os
 
 # Build library/binary/hex. can be [y, n]
 BUILD_LIB	?= y
@@ -64,6 +64,12 @@ USE_CACHE		?=n
 
 # SHA-1 class
 USE_SHA_1		?=n
+
+# PolarSSL
+USE_POLARSSL	?=n
+
+# AES TMOS
+USE_AES_TMOS	?=y
 
 
 
