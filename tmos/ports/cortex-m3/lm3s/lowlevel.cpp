98d9d33790724e60ffed94a93c59118af768a001
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-01-31 15:43:00 +0000

    LowLevel reset and reboot

--- tmos/ports/cortex-m3/lm3s/lowlevel.cpp
+++ tmos/ports/cortex-m3/lm3s/lowlevel.cpp
@@ -9,13 +9,46 @@
 #include <tmos.h>
 #include <sysctl_lm3s.h>
 #include <memmap_inc.h>
+#include <cmsis_cpp.h>
 
 /**
  *  Low Level init for LM3S
  */
-extern "C" __attribute__ ((weak)) void LowLevelInit( void )
+WEAK_C void LowLevelInit( void )
 {
 	SYSCTL->SysCtlClockSet(SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_OSC
 			| SYSCTL_SYSDIV_2);
 	system_clock_frequency = SYSCTL->SysCtlClockGet();
+}
+
+/**
+ * Low level peripheral reset
+ * Can be replaced by app
+ */
+WEAK_C void LowLevelReset( void)
+{
+	SYSCTL->SRCR1 = -1;	// reset peripherals
+	SYSCTL->SRCR1 = 0;	// reset peripherals
+}
+
+/**
+ * Reboots the device
+ * Can be replaced by app
+ */
+WEAK_C void LowLevelReboot( void)
+{
+	SCB->SysCtlReset();
+}
+
+/**
+ * Reset cause
+ * @return device specific value for the cause
+ */
+WEAK_C unsigned int LowLevelResetCause(void)
+{
+	unsigned int res;
+
+	res = SYSCTL->SysCtlResetCauseGet();
+	SYSCTL->SysCtlResetCauseClear(res);
+	return res;
 }
