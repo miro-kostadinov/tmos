01d0f3b6791b98f13de6f5008a53c86b62070f87
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-02-03 09:21:18 +0000

    AT91 USB

--- tmos/boards/ron200/brd_cfg.h
+++ tmos/boards/ron200/brd_cfg.h
@@ -18,4 +18,22 @@
 #define USE_TIME			0
 #define USE_TMOS_STDIO		1
 
+
+/*----------------------------------------------------------------------------*/
+/**
+ * \page sam3s_ek_usb "SAM3S-EK - USB device"
+ *
+ * \section Definitions
+ * - \ref BOARD_USB_BMATTRIBUTES
+ * - \ref CHIP_USB_UDP
+ * - \ref CHIP_USB_PULLUP_INTERNAL
+ * - \ref CHIP_USB_NUMENDPOINTS
+ * - \ref CHIP_USB_ENDPOINTS_MAXPACKETSIZE
+ * - \ref CHIP_USB_ENDPOINTS_BANKS
+ */
+
+/** USB attributes configuration descriptor (bus or self powered, remote wakeup) */
+#define BOARD_USB_BMATTRIBUTES    USBConfigurationDescriptor_SELFPOWERED_RWAKEUP
+
+
 #endif /* BRD_CFG_H_ */
