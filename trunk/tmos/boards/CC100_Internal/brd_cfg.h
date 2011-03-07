565db4e0eee839974d138020b2eb3bd2fcdc62fc
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-03-07 13:08:57 +0000

    internal gprs modem

---
+++ trunk/tmos/boards/CC100_Internal/brd_cfg.h
@@ -0,0 +1,26 @@
+/*
+ * tmos_cfg.h
+ *
+ *  Created on: 10.11.2010
+ *      Author: Miroslav Kostadinov
+ */
+
+
+//* TMOS related configurations
+
+#ifndef BRD_CFG_H_
+#define BRD_CFG_H_
+
+#define USE_TMOS_STRING		1
+#define USE_CSTRING			1
+#define USE_TMOS_STDLIB		1
+#define USE_TIME			1
+#define USE_TMOS_STDIO		1
+#define USE_CRC32			1
+#define USE_CACHE			1
+#define USE_SHA_1			1
+#define USE_GPIO_EXPANDER 	0
+
+#define USE_EXCEPTION_RECORD 1
+
+#endif /* BRD_CFG_H_ */
