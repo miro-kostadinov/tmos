5ba38a589b461fffc5ce1bd9d0c699a1eb345353
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-05-17 09:10:58 +0000

    LM3S Flash write user regs

--- trunk/tmos/hardware/lm3s/module.mk
+++ trunk/tmos/hardware/lm3s/module.mk
@@ -20,6 +20,7 @@ local_h_src-y 	+= fam_cpp.h fam_inc.h memmap_inc.h sysctl_lm3s.h uart_lm3s.h
 local_h_src-y 	+= gpio_lm3s.h udma_lm3s.h ssi_lm3s.h adc_lm3s.h gpio_inc.h 
 local_h_src-y 	+= ssi_inc.h uart_inc.h sysctl_inc.h types_lm3s.h timer_inc.h
 local_h_src-y 	+= timer_lm3s.h flash_lm3s.h ethernet_lm3s.h ethernet_inc.h
+local_h_src-y 	+= rom_lm3s.h 
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
