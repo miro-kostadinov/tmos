fbf930b1998946b09885f5bde46960d710217ce9
miro.kostadinov <miro.kostadinov@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-07-14 18:54:47 +0000

    enable usb for the board

--- tmos/boards/cc100eth/module.mk
+++ tmos/boards/cc100eth/module.mk
@@ -10,7 +10,7 @@ OUT_NAME	?= cc100eth
 OUT_DIR		?= out/$(OUT_NAME)/
 
 #  optimisation level  can be [0, 1, 2, 3, s].
-OPT			?= -O0
+OPT			?= -O1
 
 # Build library/binary/hex. can be [y, n]
 BUILD_LIB	?= y
@@ -68,6 +68,9 @@ USE_SHA_1		?=y
 
 # GUI
 USE_GUI			?=y
+
+# USB
+USE_USB			?=y
 
 # GPIO output expander (shift register)
 USE_GPIO_EXPANDER ?=y
