171446de9de6d43bcd9e05212378283f26174528
bratkov <bratkov@netissat.bg>
2011-09-30 15:23:33 +0000

    locked set/clr int

--- tmos/ports/cortex-m3/services.h
+++ tmos/ports/cortex-m3/services.h
@@ -34,6 +34,8 @@ int  locked_add_int(volatile int* Addend, int);
 int  locked_dec_int(volatile int* Addend);
 int  locked_set_byte(volatile unsigned char* var, unsigned int flags);
 int  locked_clr_byte(volatile unsigned char* var, unsigned int flags);
+unsigned int  locked_set_int(volatile unsigned int* var, unsigned int flags);
+unsigned int  locked_clr_int(volatile unsigned int* var, unsigned int flags);
 void* locked_set_if_null(void* pointer, void* value);
 
 #ifdef __cplusplus
