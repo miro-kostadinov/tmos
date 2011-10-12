4a523c38c50bbe1825c2101687566dec90e4eb04
miro.kostadinov@gmail.com <miro.kostadinov@gmail.com@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2011-10-12 07:28:39 +0000

    tmos_vsprintf()

--- trunk/tmos/ports/cortex-m3/tmos_stdio.h
+++ trunk/tmos/ports/cortex-m3/tmos_stdio.h
@@ -8,11 +8,15 @@
 #ifndef TMOS_STDIO_H_
 #define TMOS_STDIO_H_
 
+#include <stdarg.h>
+
 #ifdef __cplusplus
  extern "C" {
 #endif
 
 int tmos_sprintf(char *buf, const char *format, ...);
+int tmos_vsprintf(char *buffer, const char *format, va_list arg);
+
 int tmos_sscanf(const char *buf, const char *format, ...);
 
 #ifdef __cplusplus
