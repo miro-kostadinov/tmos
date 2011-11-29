534342c05cde589327ccf63f459c71fb1eb21951
bratkov <bratkov@netissat.bg>
2011-11-29 12:37:40 +0000

    ver 1.11.8.7 cc100

---
+++ 1.11.8.7/drivers/at91/cortex-m3/sam3s/series_drv.h
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ * @ingroup	drv_at91_sam3s
+ * @file	drivers/at91/cortex-m3/sam3s/series_drv.h
+ * @brief  	AT91 SAM3S drivers header
+ * @version V3.00
+ * @date    12. December 2010
+ * @author	Miroslav Kostadinov
+ *
+ * @defgroup drv_at91_sam3s AT91 SAM3S Drivers
+ * @ingroup	 drv_at91_cm3
+ * Drivers for SAM3S series from the AT91 family
+ *
+ ******************************************************************************/
+#ifndef SERIES_DRV_H_
+#define SERIES_DRV_H_
+
+
+#endif /* SERIES_DRV_H_ */
