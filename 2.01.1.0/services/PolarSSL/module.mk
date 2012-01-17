104b4c68f05d1118ef6b093b423fb5e2508522e4
bratkov <bratkov@netissat.bg>
2012-01-17 14:41:23 +0000

    ver 2.01.1.0 cc100eth

---
+++ 2.01.1.0/services/PolarSSL/module.mk
@@ -0,0 +1,33 @@
+##########################################################################
+#
+#	Make include for PolarSSL
+#	
+#   Platform: MPOS rev.3.0	
+#
+#
+##########################################################################
+
+# local sources
+local_as_src-y	:=
+local_c_src-y 	:= 
+local_cpp_src-y	:=   
+local_h_src-y 	:=
+
+local_c_src-y 	+= aes.c 
+
+local_h_src-y 	+= aes.h
+
+#updating global variables
+as_sources 	+= $(call changepath,$(local_as_src-y))
+c_sources  	+= $(call changepath,$(local_c_src-y))
+cpp_sources += $(call changepath,$(local_cpp_src-y))
+h_sources  	+= $(call changepath,$(local_h_src-y))
+
+
+#submodules
+local_modules-y := 
+
+sub_modules := $(call changepath,$(local_modules-y))
+modules += $(sub_modules)
+
+include $(addsuffix /module.mk,$(sub_modules))
