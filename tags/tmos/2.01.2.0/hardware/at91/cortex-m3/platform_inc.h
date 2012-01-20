c8b4c18a476044d26a47201bf7c3dd042e6b34ea
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2012-01-20 12:50:16 +0000

    ver 2.01.2.0 cc100

---
+++ tags/tmos/2.01.2.0/hardware/at91/cortex-m3/platform_inc.h
@@ -0,0 +1,22 @@
+/**************************************************************************//**
+ * @file     hardware/at91/cortex-m3/platform_inc.h
+ * @ingroup	 at91_cortex_m3
+ * @brief    AT91 FAMILY inc header
+ * @version  V3.00
+ * @date     11. November 2010
+ * @author	 Miroslav Kostadinov
+ *
+ *
+ * @defgroup at91_cortex_m3 Cortex-m3 platform
+ * @ingroup	 hardware_at91
+ * Source and definitions for Cortex M3 based mcu's from Atmel
+ *
+ *
+ ******************************************************************************/
+
+#ifndef PLATFORM_INC_H_
+#define PLATFORM_INC_H_
+
+#include <series_inc.h>
+
+#endif /* PLATFORM_INC_H_ */
