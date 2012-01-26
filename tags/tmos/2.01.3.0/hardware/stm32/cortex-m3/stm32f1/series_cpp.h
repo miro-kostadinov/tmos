d6940c5546ba02c545798d974b88e6b6f7d004d2
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2012-01-26 14:32:18 +0000

    ver 2.01.3.0 cc100

---
+++ tags/tmos/2.01.3.0/hardware/stm32/cortex-m3/stm32f1/series_cpp.h
@@ -0,0 +1,24 @@
+/**************************************************************************//**
+ * @ingroup	 hardware_stm32f1
+ * @{
+ * @file     hardware/stm32/cortex-m3/stm32f1/series_cpp.h
+ * @brief    STM32F1 series CPP header
+ * @version  V3.00
+ * @date     13. December 2011
+ * @author	 Miroslav Kostadinov
+ *
+ *
+ *
+ ******************************************************************************/
+#ifndef SERIES_CPP_H_
+#define SERIES_CPP_H_
+
+#include "series_inc.h"
+#include <tmos_types.h>
+
+#include "mcu_cpp.h"
+
+
+
+#endif /* SERIES_CPP_H_ */
+/** @} ingroup hardware_sam3s */
