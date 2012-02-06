c0d71390180b320f1e33ee9b73f22e87854f687e
miro.kostadinov <miro.kostadinov@gmail.com>
2012-02-06 13:07:55 +0000

    net & Sockets

--- tmos/services/net/module.mk
+++ tmos/services/net/module.mk
@@ -1,24 +1,23 @@
 ##########################################################################
 #
-#	Make include for services
+#	Make include for services/net
 #	
-#   Platform: MPOS rev.3.0	
+#   Platform: MPOS rev.2.3
 #
+#   www.thumbos.com
 #
 ##########################################################################
 
 # local sources
 local_as_src-y	:=
-local_c_src-y 	:= 
-local_cpp_src-y	:=   
+local_c_src-y 	:=
+local_cpp_src-y	:=
 local_h_src-y 	:=
 
-local_as_src-$(USE_AES_TMOS)+= aes_tmos.S
+local_cpp_src-y	+= CSocket.cpp 
 
-local_cpp_src-$(USE_SHA_1) 	+= sha_1.cpp
+local_h_src-y 	+= CSocket.h
 
-local_h_src-$(USE_SHA_1) 	+= sha_1.h
-local_h_src-$(USE_AES_TMOS)	+= aes_tmos.h
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
@@ -30,11 +29,7 @@ h_sources  	+= $(call changepath,$(local_h_src-y))
 #submodules
 local_modules-y := 
 
-local_modules-$(USE_POLARSSL)	+= PolarSSL
-local_modules-$(USE_GUI)		+= GUI
-local_modules-$(USE_USB)		+= USB
-local_modules-$(USE_LWIP_1_4)	+= lwip-1.4.0.rc1
-local_modules-$(USE_NET)		+= net
+local_modules-$(USE_LWIP_1_4_)	+= lwip-1.4.0.1
 
 sub_modules := $(call changepath,$(local_modules-y))
 modules += $(sub_modules)
