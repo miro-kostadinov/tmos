366f5faa729c345557df02c173f6aa2a7e658df1
miro.kostadinov <miro.kostadinov@gmail.com>
2012-02-22 10:49:14 +0000

    sockets and url

--- tmos/services/net/csocket.h
+++ tmos/services/net/csocket.h
@@ -1,5 +1,5 @@
 /*
- * CSocket.h
+ * csocket.h
  *
  *  Created on: Jan 31, 2012
  *      Author: Miroslav Kostadinov
@@ -9,6 +9,7 @@
 #define CSOCKET_H_
 
 #include <tmos.h>
+#include "url.h"
 
 /// Socket commands
 /// must be implemented from each socket compatible driver
@@ -20,6 +21,30 @@
 #define SOCK_CMD_CONNECT_URL	(( 6 <<4)+CMD_COMMAND)
 #define SOCK_CMD_ACCEPT			(( 7 <<4)+CMD_COMMAND)
 #define SOCK_CMD_CLOSE			(( 8 <<4)+CMD_COMMAND)
+#define SOCK_CMD_GET_HOST		(( 9 <<4)+CMD_COMMAND)
+
+#define SOCKET_CLOSED		0
+#define SOCKET_OPEN			1
+#define SOCKET_CONECTED		3
+
+#define SOCKET_ID_INVALID	0xffff
+
+struct sock_mode
+{
+	DRIVER_INDEX	driver;
+};
+
+struct apn_stru
+{
+	CSTRING name;
+	CSTRING user;
+	CSTRING pass;
+};
+
+struct sock_gprs_mode: public sock_mode
+{
+	apn_stru* apn;
+};
 
 /**
  * CSocket class
@@ -27,13 +52,21 @@
  */
 struct CSocket: public CHandle
 {
+	unsigned short sock_state;
+	unsigned short sock_id;
+
+	CSocket(): CHandle(), sock_state(SOCKET_CLOSED), sock_id(SOCKET_ID_INVALID) {};
+	~CSocket() { close(); };
+
+	bool open(const sock_mode* smode);
 	RES_CODE bind(unsigned int ip_adr, unsigned int port);
 	RES_CODE bind(const char* url);
 	RES_CODE listen(int backlog);
-	RES_CODE connect(unsigned int ip_adr, unsigned int port);
+	RES_CODE connect(const char* ip_adr, unsigned int port);
+	RES_CODE connect(CURL& link);
 	RES_CODE connect(const char* url);
 	CSocket* accept(unsigned int timeout = 1024*1024);
-
+	RES_CODE gethostbyname(CSTRING& ip_adr, const char* url);
 	RES_CODE close();
 };
 
