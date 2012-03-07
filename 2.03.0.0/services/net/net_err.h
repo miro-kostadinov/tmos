1de8fbdf2d409e8d330df77fdf938376d3b83b1c
bratkov <bratkov@netissat.bg>
2012-03-07 15:37:46 +0000

    ver 2.03.0.0 cc100

---
+++ 2.03.0.0/services/net/net_err.h
@@ -0,0 +1,39 @@
+/*
+ * net_err.h
+ *
+ *  Created on: Mar 1, 2012
+ *      Author: miro
+ */
+
+#ifndef NET_ERR_H_
+#define NET_ERR_H_
+
+
+typedef unsigned int  NET_CODE;
+
+//-----------------			 common error codes				 -----------------//
+#define NET_IDLE					0x00	//!< No result is available yet
+#define NET_OK						0x01	//!< Success (no errors)
+#define NET_EOF						0x03	//!< Reached End of File
+#define NET_ERROR					0x10	//!< Logical or Unknown error
+#define NET_ERR_OUT_OF_MEMORY		0x14	//!< Out of memory
+#define NET_ERR_HANDLE_OPEN			0x81	//!< Failed to open handle
+#define NET_BAD_DATA_RECEIVED		0x82	//!< Received bad data
+
+//-----------------			Socket error codes				 -----------------//
+#define NET_ERR_PHY_NOT_READY		0x90	//!< Physical level (cable or modem)
+#define NET_ERR_SOCK_CLOSED			0x91	//!< Socket is closed
+#define NET_ERR_SOCK_NOT_CLOSED		0x92	//!< Socket is not closed
+#define NET_ERR_SOCK_CREATE			0x93	//!< Failed to get socket
+#define NET_ERR_SOCK_CONNECT		0x94	//!< Failed to connect socket
+
+//-----------------		URL error codes						 -----------------//
+#define NET_ERR_URL_SCHEME			0xA0	//!< BAD or unsupported URL scheme
+#define NET_ERR_URL_INVALID			0xA1	//!< URL is invalid
+#define NET_ERR_URL_DNS				0xA2	//!< Failed to resolve the domain address
+#define NET_ERR_HTTP_CODE			0xA3	//!< HTTP code is not 200 OK
+
+
+
+
+#endif /* NET_ERR_H_ */
