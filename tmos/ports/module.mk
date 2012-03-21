8efb756fa391b1a69b547ae5f88d774fc73bbdfb
miro.kostadinov <miro.kostadinov@gmail.com>
2012-03-21 09:21:08 +0000

    crc16

--- tmos/ports/module.mk
+++ tmos/ports/module.mk
@@ -13,13 +13,18 @@ local_c_src-y 	:=
 local_cpp_src-y	:=   
 local_h_src-y 	:=
 
+local_as_src-$(USE_CRC16)		+= crc16.S
+
 local_c_src-$(USE_CRC32)		+= crc32.c
 
 local_cpp_src-$(USE_CSTRING)	+= str_list.cpp
 local_cpp_src-$(USE_CACHE)		+= cache.cpp
 
-local_h_src-y 	+= tmos.h tmos_cfg.h tmos_inc.h str_list.h tmos_types.h crc32.h
-local_h_src-y 	+= cache.h memory.h mqueue.h
+local_h_src-y 	+= tmos.h tmos_cfg.h tmos_inc.h tmos_types.h memory.h mqueue.h 
+local_h_src-$(USE_CSTRING)		+= str_list.h
+local_h_src-$(USE_CACHE)		+= cache.h
+local_h_src-$(USE_CRC32)		+= crc32.h
+local_h_src-$(USE_CRC16)		+= crc16.h
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
