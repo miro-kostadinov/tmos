651e51cb12bf5accc2d71cca195618220e22056e
miro.kostadinov <miro.kostadinov@gmail.com>
2012-05-03 12:09:49 +0000

    int64_t and uint64_t

--- tmos/ports/tmos_types.h
+++ tmos/ports/tmos_types.h
@@ -23,6 +23,8 @@
 #define WEAK_C 			EXTERN_C WEAK
 
 typedef unsigned int size_t;
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
 typedef unsigned int uint32_t;
 typedef int int32_t;
 typedef unsigned short uint16_t;
