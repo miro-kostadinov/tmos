3f77fa2b05297d99fe993db89a03c60b7f014a66
miro.kostadinov@gmail.com <miro.kostadinov@gmail.com@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2012-11-13 13:30:53 +0000

    fpp100/STM32F2 target

---
+++ trunk/demo_evals/targets/fpp100/drivers.cpp
@@ -0,0 +1,323 @@
+/*
+ * drivers.cpp
+ *
+ *  Created on: Nov 12, 2012
+ *      Author: miro
+ */
+#include <tmos.h>
+#include <drivers.h>
+#include <systick_drv.h>
+
+const char restart_on_exception =0;
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		 DEFAULT DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+void DEFAULT_DRIVER_DCR(DRIVER_INFO drv_info, unsigned int reason, void * param)
+{
+}
+
+void DEFAULT_DRIVER_DSR(DRIVER_INFO drv_info, HANDLE hnd)
+{
+}
+
+void DEFAULT_DRIVER_ISR(DRIVER_INFO drv_info)
+{
+	while(1);
+}
+
+const DRIVER_INFO_Type DefaultDriver =
+{
+		DRIVER_INFO_STUB,
+		DEFAULT_DRIVER_ISR,
+		DEFAULT_DRIVER_DCR,
+		DEFAULT_DRIVER_DSR,
+		INALID_DRV_INDX,
+		DRV_PRIORITY_KERNEL,
+		ID_NO_PERIPH
+};
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 SYSTICK DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+extern "C" const SYSTICK_DRIVER_INFO systick_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)isr_ClockDriver,
+			(DRV_DCR)SYSTICK_DCR,
+			(DRV_DSR)DEFAULT_DRIVER_DSR,	//!< cannot be used!
+			SysTick_IRQn,
+			DRV_PRIORITY_KERNEL,
+			ID_NO_PERIPH
+		},
+		1									//!< OS_QUANTUM_PERIOD = 1 ms
+};
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 GPIO / EXTI DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+EXTI_DRIVER_DATA gpio_data;
+const EXTI_DRIVER_INFO exti0_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI0_IRQn,
+			DRV_PRIORITY_EXTI0,
+			ID_PERIPH_SYSC
+		},
+		EXTI,
+		EXTI_IMR_MR0,
+		&gpio_data
+};
+
+const EXTI_DRIVER_INFO exti1_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI1_IRQn,
+			DRV_PRIORITY_EXTI1,
+			ID_NO_PERIPH
+		},
+		EXTI,
+		EXTI_IMR_MR1,
+		&gpio_data
+};
+
+const EXTI_DRIVER_INFO exti2_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI2_IRQn,
+			DRV_PRIORITY_EXTI2,
+			ID_NO_PERIPH
+		},
+		EXTI,
+		EXTI_IMR_MR2,
+		&gpio_data
+};
+
+const EXTI_DRIVER_INFO exti3_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI3_IRQn,
+			DRV_PRIORITY_EXTI3,
+			ID_NO_PERIPH
+		},
+		EXTI,
+		EXTI_IMR_MR3,
+		&gpio_data
+};
+
+const EXTI_DRIVER_INFO exti4_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI4_IRQn,
+			DRV_PRIORITY_EXTI4,
+			ID_NO_PERIPH
+		},
+		EXTI,
+		EXTI_IMR_MR4,
+		&gpio_data
+};
+
+const EXTI_DRIVER_INFO exti5_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI9_5_IRQn,
+			DRV_PRIORITY_EXTI5,
+			ID_NO_PERIPH
+		},
+		EXTI,
+		EXTI_IMR_MR5 | EXTI_IMR_MR6 | EXTI_IMR_MR7 | EXTI_IMR_MR8 | EXTI_IMR_MR9,
+		&gpio_data
+};
+
+const EXTI_DRIVER_INFO exti10_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)EXTI_ISR,
+			(DRV_DCR)EXTI_DCR,
+			(DRV_DSR)EXTI_DSR,
+			EXTI15_10_IRQn,
+			DRV_PRIORITY_EXTI10,
+			ID_NO_PERIPH
+		},
+		EXTI,
+		EXTI_IMR_MR10 | EXTI_IMR_MR11 | EXTI_IMR_MR12 | EXTI_IMR_MR13 | EXTI_IMR_MR15 | EXTI_IMR_MR15,
+		&gpio_data
+};
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 UART 2 DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const USART_DRIVER_MODE uart_default_mode =
+{
+	115200,									//!< baudrate e.g. 9600
+	USART_CR1_RE | USART_CR1_TE | USART_CR1_IDLEIE | USART_CR1_RXNEIE |
+		USART_CR1_TXEIE | USART_CR1_UE,		//!< USART_CR1 register value
+	USART_CR2_STOP_1b,						//!< USART_CR2 register value
+	0										//!< USART_CR3 register value
+};
+
+USART_DRIVER_DATA uart1_drv_data;
+const USART_DRIVER_INFO uart1_driver =
+{
+		{
+			DRIVER_INFO_STUB,
+			(DRV_ISR)USART_ISR,
+			(DRV_DCR)USART_DCR,
+			(DRV_DSR)USART_DSR,
+			USART1_IRQn,
+			DRV_PRIORITY_UART1,
+			ID_PERIPH_USART1
+		},
+		USART1,
+		{(PIN_DESC)PIN_UART1_RX, (PIN_DESC)PIN_UART1_TX, 0, 0, 0},      // pin Rx , pin Tx, pin RTS, pin CTS
+		&uart1_drv_data
+};
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 SPIO DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 USB DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+extern signed char const DRV_RESET_FIRST_TABLE[] =
+{
+	SysTick_IRQn,
+	INALID_DRV_INDX
+};
+
+//=================== DRV_TABLE ==========================================
+// All drivers in the system must be listed in this table
+// To open a driver, only the driver index is required (defined in drivers.h)
+// The driver index must match with the driver position in this table
+extern "C" char * const DRV_TABLE[INALID_DRV_INDX+1] __attribute__ ((section (".DriverTable")))  =
+{
+    /******  STM32 F2 specific Interrupt Numbers ***********************************************************/
+    1+ (char * const)&DefaultDriver, 	/*!< 0  Window WatchDog Interrupt                                         */
+    1+ (char * const)&DefaultDriver, 	/*!< 1  PVD through EXTI Line detection Interrupt                         */
+    1+ (char * const)&DefaultDriver, 	/*!< 2  Tamper and TimeStamp interrupts through the EXTI line             */
+    1+ (char * const)&DefaultDriver, 	/*!< 3  RTC Wakeup interrupt through the EXTI line                        */
+    1+ (char * const)&DefaultDriver, 	/*!< 4  FLASH global Interrupt                                            */
+    1+ (char * const)&DefaultDriver, 	/*!< 5  RCC global Interrupt                                              */
+    1+ (char * const)&exti0_driver, 	/*!< 6  EXTI Line0 Interrupt                                              */
+    1+ (char * const)&exti1_driver, 	/*!< 7  EXTI Line1 Interrupt                                              */
+    1+ (char * const)&exti2_driver, 	/*!< 8  EXTI Line2 Interrupt                                              */
+    1+ (char * const)&exti3_driver, 	/*!< 9  EXTI Line3 Interrupt                                              */
+    1+ (char * const)&exti4_driver, 	/*!< 10 EXTI Line4 Interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 11 DMA1 Stream 0 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 12 DMA1 Stream 1 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 13 DMA1 Stream 2 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 14 DMA1 Stream 3 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 15 DMA1 Stream 4 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 16 DMA1 Stream 5 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 17 DMA1 Stream 6 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 18 ADC1, ADC2 and ADC3 global Interrupts                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 19 CAN1 TX Interrupt                                                 */
+    1+ (char * const)&DefaultDriver, 	/*!< 20 CAN1 RX0 Interrupt                                                */
+    1+ (char * const)&DefaultDriver, 	/*!< 21 CAN1 RX1 Interrupt                                                */
+    1+ (char * const)&DefaultDriver, 	/*!< 22 CAN1 SCE Interrupt                                                */
+    1+ (char * const)&exti5_driver, 	/*!< 23 External Line[9:5] Interrupts                                     */
+    1+ (char * const)&DefaultDriver, 	/*!< 24 TIM1 Break interrupt and TIM9 global interrupt                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 25 TIM1 Update Interrupt and TIM10 global interrupt                  */
+    1+ (char * const)&DefaultDriver, 	/*!< 26 TIM1 Trigger and Commutation Interrupt and TIM11 global interrupt */
+    1+ (char * const)&DefaultDriver, 	/*!< 27 TIM1 Capture Compare Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 28 TIM2 global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 29 TIM3 global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 30 TIM4 global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 31 I2C1 Event Interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 32 I2C1 Error Interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 33 I2C2 Event Interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 34 I2C2 Error Interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 35 SPI1 global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 36 SPI2 global Interrupt                                             */
+    1+ (char * const)&uart1_driver, 	/*!< 37 USART1 global Interrupt                                           */
+    1+ (char * const)&DefaultDriver, 	/*!< 38 USART2 global Interrupt                                           */
+    1+ (char * const)&DefaultDriver, 	/*!< 39 USART3 global Interrupt                                           */
+    1+ (char * const)&exti10_driver, 	/*!< 40 External Line[15:10] Interrupts                                   */
+    1+ (char * const)&DefaultDriver, 	/*!< 41 RTC Alarm (A and B) through EXTI Line Interrupt                   */
+    1+ (char * const)&DefaultDriver, 	/*!< 42 USB OTG FS Wakeup through EXTI line interrupt                     */
+    1+ (char * const)&DefaultDriver, 	/*!< 43 TIM8 Break Interrupt and TIM12 global interrupt                   */
+    1+ (char * const)&DefaultDriver, 	/*!< 44 TIM8 Update Interrupt and TIM13 global interrupt                  */
+    1+ (char * const)&DefaultDriver, 	/*!< 45 TIM8 Trigger and Commutation Interrupt and TIM14 global interrupt */
+    1+ (char * const)&DefaultDriver, 	/*!< 46 TIM8 Capture Compare Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 47 DMA1 Stream7 Interrupt                                            */
+    1+ (char * const)&DefaultDriver, 	/*!< 48 FSMC global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 49 SDIO global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 50 TIM5 global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 51 SPI3 global Interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 52 UART4 global Interrupt                                            */
+    1+ (char * const)&DefaultDriver, 	/*!< 53 UART5 global Interrupt                                            */
+    1+ (char * const)&DefaultDriver, 	/*!< 54 TIM6 global and DAC1&2 underrun error  interrupts                 */
+    1+ (char * const)&DefaultDriver, 	/*!< 55 TIM7 global interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 56 DMA2 Stream 0 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 57 DMA2 Stream 1 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 58 DMA2 Stream 2 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 59 DMA2 Stream 3 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 60 DMA2 Stream 4 global Interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 61 Ethernet global Interrupt                                         */
+    1+ (char * const)&DefaultDriver, 	/*!< 62 Ethernet Wakeup through EXTI line Interrupt                       */
+    1+ (char * const)&DefaultDriver, 	/*!< 63 CAN2 TX Interrupt                                                 */
+    1+ (char * const)&DefaultDriver, 	/*!< 64 CAN2 RX0 Interrupt                                                */
+    1+ (char * const)&DefaultDriver, 	/*!< 65 CAN2 RX1 Interrupt                                                */
+    1+ (char * const)&DefaultDriver, 	/*!< 66 CAN2 SCE Interrupt                                                */
+    1+ (char * const)&DefaultDriver, 	/*!< 67 USB OTG FS global Interrupt                                       */
+    1+ (char * const)&DefaultDriver, 	/*!< 68 DMA2 Stream 5 global interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 69 DMA2 Stream 6 global interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 70 DMA2 Stream 7 global interrupt                                    */
+    1+ (char * const)&DefaultDriver, 	/*!< 71 USART6 global interrupt                                           */
+    1+ (char * const)&DefaultDriver, 	/*!< 72 I2C3 event interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 73 I2C3 error interrupt                                              */
+    1+ (char * const)&DefaultDriver, 	/*!< 74 USB OTG HS End Point 1 Out global interrupt                       */
+    1+ (char * const)&DefaultDriver, 	/*!< 75 USB OTG HS End Point 1 In global interrupt                        */
+    1+ (char * const)&DefaultDriver, 	/*!< 76 USB OTG HS Wakeup through EXTI interrupt                          */
+    1+ (char * const)&DefaultDriver, 	/*!< 77 USB OTG HS global interrupt                                       */
+    1+ (char * const)&DefaultDriver, 	/*!< 78 DCMI global interrupt                                             */
+    1+ (char * const)&DefaultDriver, 	/*!< 79 CRYP crypto global interrupt                                      */
+    1+ (char * const)&DefaultDriver, 	/*!< 80 Hash and Rng global interrupt                                     */
+    1+ (char * const)&DefaultDriver, 		//81 key drv
+    1+ (char * const)&DefaultDriver, 		//82 gui drv
+
+
+   NULL				//null terminated list
+};
+
+
+
+
+
---
+++ trunk/demo_evals/targets/fpp100/drivers.h
@@ -0,0 +1,201 @@
+/*
+ * drivers.h
+ *
+ *  Created on: Nov 12, 2012
+ *      Author: miro
+ */
+
+#ifndef DRIVERS_H_
+#define DRIVERS_H_
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 target header files...
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <tmos.h>
+#include <hardware_cpp.h>
+
+
+extern "C" char __BUILD_VERSION;
+
+#define LOCK_DEVICE 0
+#define RELEASE 0
+
+//----- Driver Priorities ----------------//
+#define DRV_PRIORITY_KERNEL		0x01	// OS-aware ISRs  must have lower priority (higher number)
+#define DRV_PRIORITY_UART0		0x02
+#define DRV_PRIORITY_UART1		0x02
+#define DRV_PRIORITY_UART2		0x02
+#define DRV_PRIORITY_EXTI0		0x03
+#define DRV_PRIORITY_EXTI1		0x03
+#define DRV_PRIORITY_EXTI2		0x03
+#define DRV_PRIORITY_EXTI3		0x03
+#define DRV_PRIORITY_EXTI4		0x03
+#define DRV_PRIORITY_EXTI5		0x03
+#define DRV_PRIORITY_EXTI10		0x03
+#define DRV_PRIORITY_SPI0		0x03
+#define DRV_PRIORITY_USB		0x03
+#define DRV_PRIORITY_WDT		0x04
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 DRIVER INFO
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//subw	r0, pc, #4
+//ldr.w	pc, [r0, #8]
+#define DRIVER_INFO_STUB  0x0004f2af, 0xf008f8d0
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 UART DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include <usart_drv.h>
+
+extern const USART_DRIVER_MODE uart_default_mode;
+
+#define UART_TEST_DRIVER USART1_IRQn
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 GPIO DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include <gpio_stm32.h>
+#include <exti_drv.h>
+
+// Port A
+#define PIN_UART4_TX	(PD_PA0  | PD_AF_OUT_2MHz)								//!< UART 4
+#define PIN_PA1			(PD_PA1  | PD_OUT)										//!< unused
+#define PIN_ETH_MDIO	(PD_PA2  | PD_AF)										//!< eth
+#define PIN_PRN2_TH		(PD_PA3  | PD_ANALOG)									//!< prn 2
+#define PIN_VPRN		(PD_PA4  | PD_ANALOG)									//!< prn
+#define PIN_SPI1_SCK	(PD_PA5  | PD_AF)										//!< spi 1
+#define PIN_SPI1_MISO	(PD_PA6  | PD_AF)										//!< spi 1
+#define PIN_ETH_CRS_DV	(PD_PA7  | PD_AF)										//!< eth
+#define PIN_PA8			(PD_PA8  | PD_OUT)										//!< unused
+#define PIN_UART1_TX	(PD_PA9  | PD_AF)										//!< uart 1
+#define PIN_UART1_RX	(PD_PA10 | PD_AF)										//!< uart 1
+#define PIN_PRN1_STB1	(PD_PA11 | PD_OUT)										//!< prn 1
+#define PIN_PRN1_LATCH	(PD_PA12 | PD_OUT)										//!< prn 1
+#define PIN_TMS			(PD_PA13 | PD_AF)										//!<
+#define PIN_TCK			(PD_PA14 | PD_AF)										//!<
+#define PIN_TDI			(PD_PA15 | PD_AF)										//!<
+
+// Port B
+#define PIN_LCD_CS0		(PD_PB0  | PD_OUT)										//!< lcd
+#define PIN_SDIO_CS		(PD_PB1  | PD_OUT)										//!< sdio
+#define PIN_PB2 		(PD_PB2  | PD_IN)										//!<
+#define PIN_TDO			(PD_PB3  | PD_AF)										//!<
+#define PIN_IO_CS		(PD_PB4  | PD_OUT)										//!< io
+#define PIN_SPI1_MOSI	(PD_PB5  | PD_AF)										//!< spi 1
+#define PIN_PRN1_PH1	(PD_PB6  | PD_OUT)										//!< prn 1
+#define PIN_PRN1_PH2	(PD_PB7  | PD_OUT) 										//!< prn 1
+#define PIN_PRN1_PH3	(PD_PB8  | PD_OUT)										//!< prn 1
+#define PIN_PRN1_PH4	(PD_PB9  | PD_OUT)										//!< prn 1
+#define PIN_SPI2_SCK	(PD_PB10 | PD_AF)										//!< spi 2
+#define PIN_ETH_TX_EN	(PD_PB11 | PD_AF)										//!< eth
+#define PIN_PB12		(PD_PB12 | PD_OUT)										//!< unused
+#define PIN_USB_VBUS	(PD_PB13 | PD_AF)										//!< usb
+#define PIN_USB_DM		(PD_PB14 | PD_AF)										//!< usb
+#define PIN_USB_DP		(PD_PB15 | PD_AF)										//!< usb
+
+// Port C
+#define PIN_PRN2_PS		(PD_PC0  | PD_IN)										//!< prn 2
+#define PIN_ETH_MDC		(PD_PC1  | PD_IN)										//!< eth
+#define PIN_PWR_ADC		(PD_PC2  | PD_ANALOG)									//!< pwr
+#define PIN_DI			(PD_PC3  | PD_AF)										//!< spi 2
+#define PIN_ETH_RXD0	(PD_PC4  | PD_AF)										//!< eth
+#define PIN_ETH_RXD1	(PD_PC5  | PD_AF)										//!< eth
+#define PIN_USART6_TX	(PD_PC6  | PD_AF)										//!< usart 6
+#define PIN_USART6_RX	(PD_PC7  | PD_IN)						 				//!< usart 6
+#define PIN_LCD_BL		(PD_PC8  | PD_OUT)                                      //!< lcd
+#define PIN_GSM_ON		(PD_PC9  | PD_OUT | PD_ACTIVE_HIGH)                     //!< gsm
+#define PIN_SPI3_SCK	(PD_PC10 | PD_AF)                                       //!< spi 3
+#define PIN_UART4_RX	(PD_PC11 | PD_AF) 					                    //!< uart 4
+#define PIN_SPI3_MOSI 	(PD_PC12 | PD_AF) 					                    //!< spi 3
+#define PIN_JP3			(PD_PC13 | PD_IN)                                       //!< jp3
+#define PIN_OSC32_IN	(PD_PC14 | PD_AF)                                       //!< osc 32khz
+#define PIN_OSC32_OUT	(PD_PC15 | PD_AF)                                       //!< osc 32khz
+
+// Port D
+#define PIN_M_D2		(PD_PD0  | PD_AF)										//!< bus
+#define PIN_M_D3		(PD_PD1  | PD_AF)										//!< bus
+#define PIN_DOPEN 		(PD_PD2  | PD_IN)										//!< dopen?
+#define PIN_PFO			(PD_PD3  | PD_IN)										//!< scard
+#define PIN_M_OE		(PD_PD4  | PD_AF)										//!< bus
+#define PIN_M_WE		(PD_PD5  | PD_AF)										//!< bus
+#define PIN_BOX			(PD_PD6  | PD_OUT | PD_ACTIVE_HIGH)						//!< box/service mode in
+#define PIN_MCS0		(PD_PD7  | PD_AF)						 				//!< bus
+#define PIN_SC_IO		(PD_PD8  | PD_AF) 		                                //!< scard
+#define PIN_SC_RST		(PD_PD9  | PD_AF)       			          			//!< scard
+#define PIN_SC_CLK		(PD_PD10 | PD_AF)                                       //!< scard
+#define PIN_M_A16		(PD_PD11 | PD_AF) 					                    //!< bus
+#define PIN_M_A17	 	(PD_PD12 | PD_AF) 					                    //!< bus
+#define PIN_M_A18		(PD_PD13 | PD_AF)                                       //!< bus
+#define PIN_M_D0		(PD_PD14 | PD_AF)                                       //!< bus
+#define PIN_M_D1		(PD_PD15 | PD_AF)                                       //!< bus
+
+// Port E
+#define PIN_KEY0		(PD_PE0  | PD_IN | PD_INT_BE)							//!< key
+#define PIN_KEY1		(PD_PE1  | PD_IN | PD_INT_BE)							//!< key
+#define PIN_PRN2_LATCH	(PD_PE2  | PD_OUT)										//!< prn 2
+#define PIN_M_A19		(PD_PE3  | PD_AF)										//!< bus
+#define PIN_M_A20		(PD_PE4  | PD_AF)										//!< bus
+#define PIN_KEY2		(PD_PE5  | PD_IN | PD_INT_BE)							//!< key
+#define PIN_KEY3		(PD_PE6  | PD_IN | PD_INT_BE)							//!< key
+#define PIN_M_D4		(PD_PE7  | PD_AF)						 				//!< bus
+#define PIN_M_D5		(PD_PE8  | PD_AF)                                       //!< bus
+#define PIN_M_D6		(PD_PE9  | PD_AF)            					        //!< bus
+#define PIN_M_D7		(PD_PE10 | PD_AF)                                       //!< bus
+#define PIN_PRN2_PPH1	(PD_PE11 | PD_OUT) 					                    //!< prn 2
+#define PIN_BUZ0	 	(PD_PE12 | PD_AF) 					                    //!< buz
+#define PIN_BUZ1		(PD_PE13 | PD_AF)                                       //!< buz
+#define PIN_PRN2_PPH2	(PD_PE14 | PD_OUT)                                      //!< prn 2
+#define PIN_PRN2_STB1	(PD_PE15 | PD_OUT)                                      //!< prn 2
+
+// Port F
+#define PIN_M_A0		(PD_PF0  | PD_AF)										//!< bus
+#define PIN_M_A1		(PD_PF1  | PD_AF)										//!< bus
+#define PIN_M_A2	 	(PD_PF2  | PD_AF)										//!< bus
+#define PIN_M_A3		(PD_PF3  | PD_AF)										//!< bus
+#define PIN_M_A4		(PD_PF4  | PD_AF)										//!< bus
+#define PIN_M_A5		(PD_PF5  | PD_AF)										//!< bus
+#define PIN_PRN1_HS		(PD_PF6  | PD_ANALOG)									//!< prn 1
+#define PIN_PRN1_PS		(PD_PF7  | PD_ANALOG)						 			//!< prn 1
+#define PIN_PRN1_CS		(PD_PF8  | PD_OUT)                                      //!< prn 1
+#define PIN_PRN1_TH		(PD_PF9  | PD_ANALOG)				                    //!< prn 1
+#define PIN_PRN2_HS		(PD_PF10 | PD_ANALOG)                                   //!< prn 2
+#define PIN_LCD_RST		(PD_PF11 | PD_OUT) 					                    //!< lcd
+#define PIN_M_A6	 	(PD_PF12 | PD_AF) 					                    //!< bus
+#define PIN_M_A7		(PD_PF13 | PD_AF)                                       //!< bus
+#define PIN_M_A8		(PD_PF14 | PD_AF)                                       //!< bus
+#define PIN_M_A9		(PD_PF15 | PD_AF)                                       //!< bus
+
+// Port G
+#define PIN_M_A10		(PD_PG0  | PD_AF)										//!< bus
+#define PIN_M_A11		(PD_PG1  | PD_AF)										//!< bus
+#define PIN_M_A12		(PD_PG2  | PD_AF)										//!< bus
+#define PIN_M_A13		(PD_PG3  | PD_AF)										//!< bus
+#define PIN_M_A14		(PD_PG4  | PD_AF)										//!< bus
+#define PIN_M_A15		(PD_PG5  | PD_AF)										//!< bus
+#define PIN_LED0		(PD_PG6  | PD_OUT | PD_ACTIVE_HIGH)						//!< led
+#define PIN_LED1		(PD_PG7  | PD_OUT | PD_ACTIVE_HIGH)						//!< led
+#define PIN_LED2		(PD_PG8  | PD_OUT | PD_ACTIVE_HIGH)                     //!< led
+#define PIN_M_CS1		(PD_PG9  | PD_AF) 					                    //!< bus
+#define PIN_LED3		(PD_PG10 | PD_OUT | PD_ACTIVE_HIGH)                     //!< led
+#define PIN_ETH_IRQ		(PD_PG11 | PD_IN) 					                    //!< eth
+#define PIN_GSM_CTS 	(PD_PG12 | PD_AF) 					                    //!< gsm (uart6 rts)
+#define PIN_ETH_TXD0	(PD_PG13 | PD_AF)                                       //!< eth
+#define PIN_ETH_TXD1	(PD_PG14 | PD_AF)                                       //!< eth
+#define PIN_GSM_RTS		(PD_PG15 | PD_AF)                                       //!< gsm (usart6 cts)
+
+#define PIN_LED		PIN_LED0	// for LED test
+#define PIN_BUTTON	PIN_KEY0
+
+extern   signed char const DRV_RESET_FIRST_TABLE[];
+extern "C" char* const DRV_TABLE[INALID_DRV_INDX+1];
+
+
+
+#endif /* DRIVERS_H_ */
