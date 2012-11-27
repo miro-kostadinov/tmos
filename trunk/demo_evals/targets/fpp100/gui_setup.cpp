29c09cfdedf4e99dd9a06ed102f84e06175625e0
miro.kostadinov@gmail.com <miro.kostadinov@gmail.com@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2012-11-27 09:25:24 +0000

    enable dual displays

--- trunk/demo_evals/targets/fpp100/gui_setup.cpp
+++ trunk/demo_evals/targets/fpp100/gui_setup.cpp
@@ -12,13 +12,25 @@
 
 extern volatile unsigned int backlight_time;
 
+#define LCD1_MODE ST7565S_RaRb_RATIO(0) + ST7565S_VOLUME(18) + ST7565S_H_FLIP
+#define LCD2_MODE ST7565S_RaRb_RATIO(3) + ST7565S_VOLUME(12) + ST7565S_H_FLIP
+
 EXTERN_C int detect_displays(GUI_DRIVER_INFO* drv_info)
 {
 	HANDLE hnd;
 
 
-	hnd = new CHandle(SPI1_IRQn, &lcd_mode_stru);
-	drv_info->lcd[0] = new ST7565S(128, 32, hnd, lcdpins);
+	//display 1
+	hnd = new CHandle(SPI1_IRQn, &lcd1_mode_stru);
+	drv_info->lcd[0] = new ST7565S(128, 32, hnd, lcd1pins, LCD1_MODE);
+
+	//display 2
+	hnd = new CHandle(SPI1_IRQn, &lcd2_mode_stru);
+	drv_info->lcd[1] = new ST7565S(128, 32, hnd, lcd2pins, LCD2_MODE);
+
+	//this is required for multiple displays...
+	drv_info->lcd[0]->display = 1;
+	drv_info->lcd[1]->display = 2;
 
 
     backlight_time = 255*1024;
