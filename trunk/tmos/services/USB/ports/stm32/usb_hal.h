83614fc6f44ac05aac05bdf7df38a08f720dfdb6
miro.kostadinov@gmail.com <miro.kostadinov@gmail.com@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2013-01-09 16:04:13 +0000

    otg endpoint and fifo config fix

--- trunk/tmos/services/USB/ports/stm32/usb_hal.h
+++ trunk/tmos/services/USB/ports/stm32/usb_hal.h
@@ -34,13 +34,11 @@ void usb_start_tx(USB_DRV_INFO drv_info, HANDLE hnd);
 void usb_start_rx(USB_DRV_INFO drv_info, HANDLE hnd, uint32_t eptnum, Endpoint *endpoint);
 
 void usb_ept_reset(USB_DRV_INFO drv_info, unsigned int eptnum);
-void usb_ack_packet(USB_TypeDef* hw_base, Endpoint* endpoint, unsigned int eptnum);
 void usb_hal_reset(USB_DRV_INFO drv_info);
 void usb_hal_configure(USB_DRV_INFO drv_info);
 void usb_hal_stall(USB_TypeDef* hw_base, unsigned int eptnum);
 void usb_hal_stall_clear(USB_TypeDef* hw_base, unsigned int eptnum);
 void usb_hal_ept_config(USB_DRV_INFO drv_info, const USBGenericDescriptor* pDescriptor);
-void usb_hal_txpktrdy(USB_TypeDef* hw_base, unsigned int eptnum, int len);
 
 void usb_hal_config_fifo(USB_DRV_INFO drv_info);
 
