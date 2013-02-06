0a72978b2a19c51a34989d86efe8161b89779d33
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2013-02-06 14:51:51 +0000

    ver 3.02.2.0 cc100

---
+++ tags/tmos/3.02.2.0/boards/cc500/brd_cfg.h
@@ -0,0 +1,81 @@
+/*
+ * tmos_cfg.h
+ *
+ *  Created on: 17.01.2012
+ *      Author: Miroslav Kostadinov
+ */
+
+
+//* TMOS related configurations
+
+#ifndef BRD_CFG_H_
+#define BRD_CFG_H_
+
+
+#define HW_KEY_VER		    2
+
+#define KEY_DOWN_CODE		0x00
+#define KEY_REPEAT_CODE		0x80
+#define KEY_UP_CODE			0x40
+#define KEY_PRESREP_MASK	0x7F
+
+#define KEY_ON				0x00
+#define KEY_LEFT			0x00
+#define KEY_DOWN			0x02
+#define KEY_UP				0x01
+#define KEY_NO				0x03
+#define KEY_RIGHT			0x03
+
+#if HW_KEY_VER == 1
+#define KEY_OK				0x04
+#define KEY_C				0x05
+#endif
+
+#if HW_KEY_VER == 2
+#define KEY_OK				0x05
+#define KEY_C				0x04
+#endif
+
+#define ENABLE_INDEX		1
+#define DISABLE_INDEX		2
+#define MENU_INDEX			ENABLE_INDEX
+
+// GUI messages
+#define WM_USER				0x01
+#define WM_DRAW				0x02
+#define WM_KEY				0x03
+#define WM_INIT				0x04
+
+#define GUI_DISPLAYS		2
+#define GUI_DEFAULT_DISP	2
+
+
+#define USE_TMOS_STRING		1
+#define USE_CSTRING			1
+#define USE_TMOS_STDLIB		1
+#define USE_TIME			1
+#define USE_TMOS_STDIO		1
+#define USE_TMOS_LLONG		1
+#define USE_CRC16			1
+#define USE_CRC32			1
+#define USE_CACHE			1
+#define USE_SHA_1			1
+#define USE_BASE32			1
+#define USE_GUI				1
+#define USE_USB				1
+#define USE_AES_TMOS		1
+#define	USE_GPIO_EXPANDER 	1
+
+#define USE_EXCEPTION_RECORD 1
+#define USE_MEMORY_TRACKING	1
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 KEY DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#define KEY_COUNT			6
+
+#define KEY_POLLING_TIME	10
+#define KEY_DEBOUNCE_TIME 	50 //30 //300
+#define KEY_REPEAT_TIME 	300//150 //1000
+
+#endif /* BRD_CFG_H_ */
---
+++ tags/tmos/3.02.2.0/ports/cortex-m3/stm32/lowlevel.cpp
@@ -0,0 +1,47 @@
+/**************************************************************************//**
+ * @ingroup	 ports_cortex
+ * @file
+ * @brief    LowLevel init for STM32
+ *
+ */
+
+
+#include <tmos.h>
+#include <fam_cpp.h>
+
+/**
+ *  Low Level init for STM32
+ */
+WEAK_C void LowLevelInit( void )
+{
+
+}
+
+/**
+ * Low level peripheral reset
+ * Can be replaced by app
+ */
+WEAK_C void LowLevelReset( void)
+{
+}
+
+/**
+ * Reboots the device
+ * Can be replaced by app
+ */
+WEAK_C void LowLevelReboot( void)
+{
+}
+
+/**
+ * Reset cause
+ * @return device specific value for the cause
+ */
+WEAK_C unsigned int LowLevelResetCause(void)
+{
+	unsigned int res;
+
+	res = RCC->RCC_CSR;
+	RCC->RCC_CSR = res;
+	return (res);
+}
