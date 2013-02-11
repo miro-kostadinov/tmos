e59fa83369fcb7aadc92b5dc20b41412a52c298d
bratkov <bratkov@netissat.bg>
2013-02-11 07:05:08 +0000

    ver 3.02.3.0 cc100

---
+++ 3.02.3.0/hardware/lm3s/LM3S5B91_C1/mcu_inc.h
@@ -0,0 +1,19 @@
+/*
+ * mcu_inc.h for LM2S5B91 rev C1
+ *
+ *  Created on: 11.11.2010
+ *      Author: Miroslav Kostadinov
+ */
+
+#ifndef MCU_INC_H_
+#define MCU_INC_H_
+
+
+#define TARGET_IS_TEMPEST_RC1	1		// Tempest rev C1
+
+#define RAM_SIZE 			0x18000
+#define FLASH_PAGE_SIZE		0x400
+
+#define MCU_RESET_CAUSE_SW  0x00000010	//SYSCTL_CAUSE_SW
+
+#endif /* MCU_INC_H_ */
