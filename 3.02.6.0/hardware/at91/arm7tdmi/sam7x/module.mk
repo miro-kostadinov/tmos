a68e7fc9ac0378ff4eddec0904f9e7f52db6697d
bratkov <bratkov@netissat.bg>
2013-02-22 13:06:51 +0000

    ver 3.02.6.0 cc100

---
+++ 3.02.6.0/hardware/at91/arm7tdmi/sam7x/module.mk
@@ -0,0 +1,32 @@
+##########################################################################
+#
+#	Make include for tmos/hardware/at91/arm7tdmi/sam7x
+#	
+#   Platform: MPOS rev.3.0	
+#
+#
+##########################################################################
+
+# local sources
+local_as_src-y	:=
+local_c_src-y 	:=
+local_cpp_src-y	:=
+local_h_src-y 	:=
+
+
+#updating global variables
+as_sources 	+= $(call changepath,$(local_as_src-y))
+c_sources  	+= $(call changepath,$(local_c_src-y))
+cpp_sources += $(call changepath,$(local_cpp_src-y))
+h_sources  	+= $(call changepath,$(local_h_src-y))
+
+#add current directory to include path
+inc_dirs += $(subdirectory) 
+
+#submodules
+local_modules-y :=  $(CFG_MCU)
+
+sub_modules := $(call changepath,$(local_modules-y))
+modules += $(sub_modules)
+
+include $(addsuffix /module.mk,$(sub_modules))
