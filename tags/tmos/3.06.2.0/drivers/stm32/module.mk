dc1a7861fb224660c156fcc9138a4ac22b4c5aa3
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2013-06-14 13:28:27 +0000

    ver 3.06.2.0 cc100

---
+++ tags/tmos/3.06.2.0/drivers/stm32/module.mk
@@ -0,0 +1,42 @@
+##########################################################################
+#
+#	Make include for drivers/stm32
+#	
+#   Platform: MPOS rev.3.0	
+#
+#
+##########################################################################
+
+# local sources
+local_as_src-y	:=
+local_c_src-y 	:=
+local_cpp_src-y	:=
+local_h_src-y 	:=
+
+local_cpp_src-y	+= exti_drv.cpp usart_drv.cpp spi_drv.cpp dma_drv.cpp 
+
+local_h_src-y 	+= tmos_drivers.h exti_drv.h usart_drv.h spi_drv.h dma_drv.h
+
+#conditional drivers
+ifeq "$(CFG_SERIES)" "stm32f2"
+local_cpp_src-y	+= rtc2_drv.cpp adc2_drv.cpp
+local_h_src-y 	+= rtc2_drv.h adc2_drv.h
+endif
+
+#updating global variables
+as_sources 	+= $(call changepath,$(local_as_src-y))
+c_sources  	+= $(call changepath,$(local_c_src-y))
+cpp_sources += $(call changepath,$(local_cpp_src-y))
+h_sources  	+= $(call changepath,$(local_h_src-y))
+
+
+#add current directory to include path
+inc_dirs += $(subdirectory) 
+
+#submodules
+local_modules-y := 
+
+sub_modules := $(call changepath,$(local_modules-y))
+modules += $(sub_modules)
+
+include $(addsuffix /module.mk,$(sub_modules))
---
+++ tags/tmos/3.06.2.0/hardware/stm32/stm32f1/dma_f1.cpp
@@ -0,0 +1,27 @@
+/*
+ * dma_f1.cpp
+ *
+ *  Created on: Nov 21, 2012
+ *      Author: miro
+ */
+
+#include <tmos.h>
+#include "fam_cpp.h"
+
+
+void stm32_dma_start(DMA_TypeDef* dmac, uint32_t indx, HANDLE hnd)
+{
+}
+
+void stm32_dma_ch_cfg(DMA_TypeDef* dmac, uint32_t indx, DMA_DRIVER_MODE* mode)
+{
+
+}
+
+void stm32_dis_ints(DMA_TypeDef* dmac, uint32_t indx)
+{
+}
+
+
+
+
