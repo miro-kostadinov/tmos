dcff93768e1c4f528d28eb9cd0fb0506fbef5a64
miro.kostadinov <miro.kostadinov@gmail.com>
2013-07-19 12:44:39 +0000

    TRACE_COLOR

--- tmos/ports/cortex-m3/services.cpp
+++ tmos/ports/cortex-m3/services.cpp
@@ -42,6 +42,27 @@ void TRACE_BUF(const void* buf, unsigned int len, unsigned int color)
 	TRACE1("\e[m");
 }
 
+void TRACE_COLOR(unsigned int color)
+{
+	TRACE1("\e[");
+	if(color <100)
+	{
+		if(color>9)
+		{
+			TRACE_CHAR('0'+ (color/10));
+			color %= 10;
+		}
+		TRACE_CHAR('0'+ color);
+	}
+	TRACE_CHAR('m');
+}
+
+void TRACE_COLOR_END()
+{
+	TRACE1("\e[m");
+}
+
+
 void TRACE_BUF(const void* buf, unsigned int len, unsigned int c1, unsigned int c2)
 {
 	TRACE1("\e[");
