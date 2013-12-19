4caa565d30e48ee439de39cf3b9a639550f40383
miro.kostadinov <miro.kostadinov@gmail.com>
2013-12-19 12:10:50 +0000

    CMSIS removed

--- tmos/ports/tmos_types.h
+++ tmos/ports/tmos_types.h
@@ -11,6 +11,8 @@
 #ifndef TMOS_TYPES_H_
 #define TMOS_TYPES_H_
 
+#ifndef __ASSEMBLY__
+
 #define __no_init		__attribute__ ((section (".ramnoinitsec")))
 
 #ifdef __cplusplus
@@ -26,6 +28,13 @@ typedef unsigned short uint16_t;
 typedef short int16_t;
 typedef unsigned char uint8_t;
 typedef char int8_t;
+union ureg_t
+{
+	uint8_t   as_u8;
+	uint16_t  as_u16;
+	uint32_t  as_u32;
+};
+
 typedef unsigned int RES_CODE;
 #ifndef min
 #define min(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
@@ -50,6 +59,13 @@ typedef unsigned short uint16_t;
 typedef short int16_t;
 typedef unsigned char uint8_t;
 typedef char int8_t;
+union ureg_t
+{
+	uint8_t   as_u8;
+	uint16_t  as_u16;
+	uint32_t  as_u32;
+};
+
 typedef unsigned int RES_CODE;
 #ifndef min
 #define min(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
@@ -77,13 +93,12 @@ typedef unsigned int RES_CODE;
 #define __O     volatile        /*!< defines 'write only' permissions     */
 #define __IO	volatile        /*!< defines 'read / write' permissions   */
 
-#ifndef __ASSEMBLY__
 #	define CAST(type, value) ((type *) (value))
 #	define REG_ACCESS(type, address) (*(type*)(address)) /**< C code: Register value */
 #else
 #	define CAST(type, value) (value)
 #	define REG_ACCESS(type, address) (address) /**< Assembly code: Register address */
-#endif
+#endif  /* __ASSEMBLY__ */
 
 #endif /* TMOS_TYPES_H_ */
 /** @} ingroup ports */
