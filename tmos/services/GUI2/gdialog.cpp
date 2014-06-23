25e64cc7269a1125b890fa84556f5aa420dde9e7
miro.kostadinov <miro.kostadinov@gmail.com>
2014-06-23 06:51:37 +0000

    object move(x,y), VSCROLL

--- tmos/services/GUI2/gdialog.cpp
+++ tmos/services/GUI2/gdialog.cpp
@@ -12,6 +12,8 @@
 
 unsigned int GDialog::process_key (GMessage& msg)
 {
+	unsigned int res;
+
 	if(GWindow::process_key(msg))
 		return 1;
 
@@ -19,16 +21,52 @@ unsigned int GDialog::process_key (GMessage& msg)
 	{
 	case KEY_RIGHT:
 	case KEY_DOWN:
-		return focus_on_next();
+		res = focus_on_next();
+		break;
 
 	case KEY_LEFT:
 	case KEY_UP:
-		return focus_on_previous();
+		res =  focus_on_previous();
+		break;
 
 	default:
-		break;
+		return 0;
 	}
-	return 0;
+	if(vscroll && focus)
+	{
+		int y, offset=0;
+		y = vpos + focus->rect.y1;
+		vscroll->SetScrollPos(GO_FLG_VSCROLL, y/10, true);
+		if(focus->rect.y1 > client_rect.y1)
+		{
+			// scroll down
+			offset = client_rect.y1 - focus->rect.y1 -5;
+		}
+		if(focus->rect.y0 < client_rect.y0 )
+		{
+			offset = client_rect.y0 -focus->rect.y0 +10;
+			// scroll up
+		}
+		if(offset != 0)
+		{
+			GObject* tmp = children;
+
+			vpos -= offset;
+			while (tmp)
+			{
+				if(tmp->flags & GO_FLG_SHOW)
+				{
+					tmp->move(0, offset);
+				}
+				tmp = tmp->nextObj;
+			}
+
+			send_message(WM_DRAW, 0, 0L, this);
+		}
+
+	}
+
+	return res;
 }
 
 unsigned int GDialog::process_command (GMessage& msg)
@@ -75,3 +113,40 @@ unsigned int GDialog::process_default (GMessage& msg)
 	return 0;
 }
 
+void GDialog::draw_this (LCD_MODULE* lcd)
+{
+	GWindow::draw_this(lcd);
+	if(vscroll)
+		vscroll->draw_scroll(lcd);
+}
+
+unsigned int GDialog::initialize (GMessage& msg)
+{
+	unsigned int res;
+
+	res = GWindow::initialize(msg);
+	if(flags & GO_FLG_VSCROLL)
+	{
+		GObject* tmp = children;
+		int maxy = client_rect.height();
+
+		while (tmp)
+		{
+			if(tmp->is_available())
+			{
+				if(maxy < tmp->client_rect.y1)
+					maxy = tmp->client_rect.y1;
+			}
+			tmp = tmp->nextObj;
+		}
+
+		if(maxy > client_rect.height())
+		{
+			if(!vscroll)
+				vscroll = new GScroll(this);
+			if(vscroll)
+				vscroll->SetScrollRange(GO_FLG_VSCROLL, maxy/10 + 1);
+		}
+	}
+	return res;
+}
--- tmos/services/GUI2/gdialog.h
+++ tmos/services/GUI2/gdialog.h
@@ -10,17 +10,35 @@
 
 #include <tmos.h>
 #include <gwindow.h>
+#include <gscroll.h>
 
 struct GDialog : GWindow
 {
-	GDialog():GWindow() {};
+	GScroll* 	vscroll;
+	int			vpos;
+
+	GDialog():GWindow(), vscroll(NULL),vpos(0) {};
 	GDialog(GId id_t, const RECT_T& rect_t,
 			unsigned char displays_t, GFlags flags_t = GO_FLG_DEFAULT)
-	:GWindow (id_t, rect_t, displays_t, flags_t) {};
+	:GWindow (id_t, rect_t, displays_t, flags_t), vscroll(NULL), vpos(0)
+	{
+	};
 
+	virtual ~GDialog()
+	{
+		if(vscroll)
+		{
+			delete vscroll;
+			vscroll = NULL;
+		}
+	}
 	virtual unsigned int process_key (GMessage& msg);
 	virtual unsigned int process_default (GMessage& msg);
 	virtual unsigned int process_command(GMessage& msg);
+
+protected:
+	virtual void draw_this (LCD_MODULE* lcd);
+	virtual unsigned int initialize (GMessage& msg);
 
 };
 
