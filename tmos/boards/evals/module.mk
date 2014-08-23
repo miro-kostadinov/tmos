bb288b19887638a49fc8690939cdb4dd098b99a2
miro.kostadinov <miro.kostadinov@gmail.com>
2014-08-23 14:43:04 +0000

    evals directory

--- tmos/boards/evals/module.mk
+++ tmos/boards/evals/module.mk
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-#	Make include for boards
+#	Make include for boards/evals
 #	
 #   Platform: MPOS rev.3.0	
 #
@@ -21,18 +21,7 @@ c_sources  	+= $(call changepath,$(local_c_src-y))
 cpp_sources += $(call changepath,$(local_cpp_src-y))
 
 #submodules
-ifeq ($(CFG_BOARD), )
-ifeq ($(CFG_EVAL), )
-else
-local_modules-y := evals
-endif
-else
-local_modules-y := $(CFG_BOARD)
-endif
-
-ifeq ($(local_modules-y), )
-$(error No board is selected! Use make CFG_BOARD=xxx or CFG_EVAL=xxx)
-endif
+local_modules-y := $(CFG_EVAL)
 
 sub_modules := $(call changepath,$(local_modules-y))
 modules += $(sub_modules)
