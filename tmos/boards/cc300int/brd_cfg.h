2c5b87911854ac1e788b724d896eca77038b1a22
bratkov <bratkov@netissat.bg>
2014-08-29 11:35:39 +0000

    target cc300int

---
+++ tmos/boards/cc300int/brd_cfg.h
@@ -0,0 +1,39 @@
+/*
+ * tmos_cfg.h
+ *
+ *  Created on: 17.01.2012
+ *      Author: Miroslav Kostadinov
+ */
+
+
+//* TMOS related configurations
+
+#ifndef BRD_CFG_H_
+#define BRD_CFG_H_
+
+#define USE_TMOS_TL_V1		1
+#define USE_CSTRING			1
+
+#define USE_TMOS_STRING		1
+#define USE_TMOS_STDLIB		1
+#define USE_TIME			1
+#define USE_TMOS_STDIO		1
+#define USE_TMOS_LLONG		1
+#define USE_CRC16			1
+#define USE_CRC32			1
+#define USE_CACHE			1
+#define USE_SHA_1			1
+#define USE_BASE32			1
+#define USE_GUI				0
+#define USE_USB				1
+#define USE_AES_TMOS		1
+#define	USE_GPIO_EXPANDER 	1
+
+#define USE_EXCEPTION_RECORD 0
+#define USE_MEMORY_TRACKING	1
+
+/// USB stack options
+#define USB_ENABLE_HOST 	0
+#define USB_ENABLE_OTG 		0
+
+#endif /* BRD_CFG_H_ */
