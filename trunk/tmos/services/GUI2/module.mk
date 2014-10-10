70d35c28cbd35cce530ba70532f68ddac5033cfd
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2014-10-10 14:45:50 +0000

    send/process message

--- trunk/tmos/services/GUI2/module.mk
+++ trunk/tmos/services/GUI2/module.mk
@@ -21,7 +21,7 @@ local_cpp_src-y	+= lcd_SPFD5414D.cpp lcd_ST7565S.cpp lcd_ST7735R.cpp lcd_ST7565S
 local_cpp_src-y += gwindow.cpp gobject.cpp gcontainer.cpp gbutton.cpp gtext.cpp
 local_cpp_src-y += gdialog.cpp gradio.cpp gedit.cpp lcd_multiplex.cpp gfloating_text.cpp
 local_cpp_src-y += gfloating_button.cpp gmenu.cpp gscroll.cpp gmsgbox.cpp
-local_cpp_src-y += glistbox.cpp 
+local_cpp_src-y += glistbox.cpp message.cpp
 
 local_h_src-y 	+= gui_drv.h lcd.h fonts.h stdgui.h lcd_EM6125.h lcd_SPFD5414D.h lcd_ST7735R.h
 local_h_src-y 	+= lcd_ST7565S.h gwindow.h gobject.h gcontainer.h gbutton.h gtext.h message.h lcd_ST7565S_RS.h
