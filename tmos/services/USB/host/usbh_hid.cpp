37ce9b32372ad528294a774f30af5739c05533f7
miro.kostadinov <miro.kostadinov@gmail.com>
2014-10-22 14:42:09 +0000

    USB HID Keyboard

--- tmos/services/USB/host/usbh_hid.cpp
+++ tmos/services/USB/host/usbh_hid.cpp
@@ -67,7 +67,7 @@ RES_CODE usb_remote_hid_t::set_report(const void* buf, uint32_t len, uint32_t re
 	RES_CODE res;
 
 	//read status
-    TRACELN1("hid: get report");
+    TRACELN1("hid: set report");
 
 	req.bmRequestType = USB_REQ_OUT_CLASS_INTERFACE;
 	req.bRequest = HIDRequest_SET_REPORT;
@@ -220,3 +220,281 @@ RES_CODE usb_remote_hid_t::scan_hid(uint32_t port_indx, USBSubClassCode subcls, 
 }
 
 
+//------------------------------------------------------------------------------
+//	usb_remote_kbd_t
+//------------------------------------------------------------------------------
+
+#define K00		255		// Error
+#define K01		255		// Error
+#define K02		255		// Error
+#define K03		255		// Error
+#define K2A		127		// DEL
+#define K39 	255		// CAPSLOCK
+#define K3A 	255		// F1
+#define K3B 	255		// F2
+#define K3C 	255		// F3
+#define K3D 	255		// F4
+#define K3E 	255		// F5
+#define K3F 	255		// F6
+#define K40 	255		// F7
+#define K41 	255		// F8
+#define K42 	255		// F9
+#define K43 	255		// F10
+#define K44 	255		// F11
+#define K45 	255		// F12
+#define K46 	255		// PrintScreen
+#define K47 	255		// SCROLLLOCK
+#define K48 	255		// PAUSE
+#define K49 	255		// INSERT
+#define K4A 	255		// HOME
+#define K4B 	255		// PAGEUP
+#define K4C 	255		// DELFWD
+#define K4D 	255		// END
+#define K4E 	255			// PAGEDOWN
+#define K4F 	KEY_RIGHT	// RIGHT
+#define K50 	KEY_LEFT    // LEFT
+#define K51 	255		    // ???
+#define K52 	KEY_UP  	//
+#define K53 	255			// Num Lock
+#define K54 	'/'			// Keypad /
+#define K55 	'*'			// Keypad *
+#define K56 	'-'			// Keypad -
+#define K57 	'+'			// Keypad +
+#define K58 	KEY_ENTER	// Keypad ENTER
+#define K59 	'1'			// Keypad 1
+#define K5A 	'2'			// Keypad 2
+#define K5B 	'3'			// Keypad 3
+#define K5C 	'4'			// Keypad 4
+#define K5D 	'5'			// Keypad 5
+#define K5E 	'6'			// Keypad 6
+#define K5F 	'7'			// Keypad 7
+#define K60 	'8'			// Keypad 8
+#define K61 	'9'			// Keypad 9
+#define K62 	'0'			// Keypad 0
+#define K63 	'.'			// Keypad .
+#define K64 	'\\' 		// Non-US \ and |
+#define K65 	255			// Application
+#define K66 	255			// Power
+#define K67 	'='			// Keypad =
+#define K68 	255			// F13
+#define K69 	255			// F14
+#define K6A 	255			// F15
+#define K6B 	255			// F16
+#define K6C 	255			// F17
+#define K6D 	255			// F18
+#define K6E 	255			// F19
+#define K6F 	255			// F20
+
+const unsigned char usb_hid_keycode[] =
+{
+	// 0    1    2    3   4    5    6    7    8    9    a    b    c    d    e    f
+	 K00, K01, K02, K03, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', //00
+	 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', //10
+	 '3', '4', '5', '6', '7', '8', '9', '0','\r','\e', K2A,'\t', ' ', '-', '=', '[', //20
+	 ']','\\', '`', ';', '\'', '`', ',', '.', '/', K39, K3A, K3B, K3C, K3D, K3E, K3F, //30
+	 K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E, K4F, //40
+	 K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A, K5B, K5C, K5D, K5E, K5F, //50
+	 K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K6A, K6B, K6C, K6D, K6E, K6F  //60
+};
+
+const unsigned char usb_hid_keycode_s[] =
+{
+	// 0    1    2    3   4    5    6    7    8    9    a    b    c    d    e    f
+	 K00, K01, K02, K03, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', //00
+	 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', //10
+	 '#', '$', '%', '^', '&', '*', '(', ')','\r','\e', K2A,'\t', ' ', '_', '+', '{', //20
+	 '}', '|', '~', ':', '"', '~', '<', '>', '?', K39, K3A, K3B, K3C, K3D, K3E, K3F, //30
+	 K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E, K4F, //40
+	 K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A, K5B, K5C, K5D, K5E, K5F, //50
+	 K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K6A, K6B, K6C, K6D, K6E, K6F  //60
+};
+
+WEAK_C void usb_kbd_post_key(unsigned int key, unsigned int flags)
+{
+	if(key <255)
+	{
+		if(flags == KEY_DOWN_CODE)
+		{
+			TRACE_COLOR(TC_TXT_BLUE);
+		} else
+			TRACE_COLOR(TC_TXT_CYAN);
+		if(key <32 )
+			TRACE("{%02x}", key);
+		else
+			TRACE_CHAR(key);
+		TRACE_COLOR_END();
+	}
+}
+
+WEAK_C unsigned int usb_scan_to_ascii(uint32_t code, uint32_t modifier, uint32_t lang)
+{
+	TRACELN("key %x:%x ", modifier, code);
+
+	if(modifier & HID_KBD_MODIFIER_RIGHT_SHIFT)
+	{
+		// process CAPSLOCK
+		if(code >= 0x04 && code <= 0x1D)
+		{
+			modifier ^= HID_KBD_MODIFIER_LEFT_SHIFT;
+		}
+		modifier ^= HID_KBD_MODIFIER_RIGHT_SHIFT;
+	}
+	//process SHIFTs only
+	switch(modifier)
+	{
+	case HID_KBD_MODIFIER_NONE:
+		if(code < sizeof(usb_hid_keycode))
+		{
+			code = usb_hid_keycode[code];
+		} else
+			code = 255;
+		break;
+
+	case HID_KBD_MODIFIER_LEFT_SHIFT:
+		if(code < sizeof(usb_hid_keycode_s))
+		{
+			code = usb_hid_keycode_s[code];
+		} else
+			code = 255;
+		break;
+
+	default:
+		TRACELN("unk key %x:%x", modifier, code);
+		code = 255;
+		break;
+	}
+	return code;
+}
+
+uint32_t usb_remote_kbd_t::get_modifier(uint32_t modifier)
+{
+	// ignore left/right
+	modifier = (modifier&0xF) | (modifier>>4);
+
+	//apply capslock
+	if(key_leds & HID_KBD_LED_CAPS_LOCK)
+		modifier |= HID_KBD_MODIFIER_RIGHT_SHIFT;
+	return modifier;
+}
+
+void usb_remote_kbd_t::key_press(uint32_t code)
+{
+
+	if(code == HID_KBD_SCAN_CAPSLOCK && new_keys.key_modifier==0)
+	{
+		key_leds2 = key_leds;
+		if(!(key_leds& HID_KBD_LED_CAPS_LOCK))
+		{
+			key_leds |= HID_KBD_LED_CAPS_LOCK;
+			set_report(&key_leds, 1, HID_REPORTTYPE_OUTPUT, 0);
+		}
+	}
+
+	old_keys.key_code[old_keys.len] = code;
+	key_tout[old_keys.len++] = 0;
+	code = usb_scan_to_ascii(code, get_modifier(new_keys.key_modifier), key_lang);
+	usb_kbd_post_key(code, KEY_DOWN_CODE);
+
+}
+
+void usb_remote_kbd_t::key_release(uint32_t code)
+{
+
+	if(code == HID_KBD_SCAN_CAPSLOCK && old_keys.key_modifier==0)
+	{
+		if(key_leds2 & HID_KBD_LED_CAPS_LOCK)
+		{
+			key_leds &= ~HID_KBD_LED_CAPS_LOCK;
+			set_report(&key_leds, 1, HID_REPORTTYPE_OUTPUT, 0);
+		}
+	}
+
+	code = usb_scan_to_ascii(code, get_modifier(old_keys.key_modifier), key_lang);
+	usb_kbd_post_key(code, KEY_UP_CODE);
+}
+
+void usb_remote_kbd_t::kbd_process()
+{
+	int i, j, k, code;
+
+	// find new len
+	for(i=5; i>=0; i--)
+		if(new_keys.key_code[i]> HID_KBD_SCAN_ERRUNDEF)
+			break;
+	new_keys.len = i+1;
+
+	if(!new_keys.len || old_keys.key_modifier != new_keys.key_modifier)
+	{
+		// release all old
+		while(old_keys.len)
+		{
+			key_release(old_keys.key_code[--old_keys.len]);
+		}
+		old_keys.key_modifier = new_keys.key_modifier;
+	}
+
+	if(new_keys.len)
+	{
+		// release old
+		j=0;
+		for(i=0; i<old_keys.len; i++)
+		{
+			code = old_keys.key_code[i];
+			for(k=0; k<new_keys.len; k++)
+			{
+				if(code == new_keys.key_code[k])
+					break;
+			}
+			if(k<new_keys.len)
+			{
+				//keep
+				old_keys.key_code[j] = code;
+				key_tout[j++] = key_tout[i];
+				new_keys.key_code[k] = HID_KBD_SCAN_NONE;
+			} else
+			{
+				//release old
+				key_release(code);
+			}
+		}
+		old_keys.len = j;
+
+		//push new
+		for(i=0; i<new_keys.len; i++)
+		{
+			code = new_keys.key_code[i];
+			if(code > HID_KBD_SCAN_ERRUNDEF)
+			{
+				key_press(code);
+			}
+		}
+	} else
+	{
+		// process ALT-SHIFT
+		switch(new_keys.key_modifier)
+		{
+		case HID_KBD_MODIFIER_LEFT_SHIFT|HID_KBD_MODIFIER_RIGHT_SHIFT|HID_KBD_MODIFIER_LEFT_CTRL:
+			if(def_lang == 1)
+			{
+				def_lang = 2;
+				if(key_lang)
+					key_lang = 2;
+			}
+			else
+			{
+				def_lang = 1;
+				if(key_lang)
+					key_lang = 1;
+			}
+			break;
+		case HID_KBD_MODIFIER_LEFT_SHIFT|HID_KBD_MODIFIER_LEFT_ALT:
+		case HID_KBD_MODIFIER_RIGHT_SHIFT|HID_KBD_MODIFIER_RIGHT_ALT:
+			if(key_lang)
+				key_lang = 0;
+			else
+				key_lang = def_lang;
+			break;
+		}
+
+	}
+}
--- tmos/services/USB/requests/usb_hid_reqs.h
+++ tmos/services/USB/requests/usb_hid_reqs.h
@@ -30,5 +30,45 @@ enum HIDGenericRequests : unsigned char
 #define HID_REPORTTYPE_FEATURE         0x03
 
 
+//------------------------------------------------------------------------------
+//     USB HID Keyboard
+//------------------------------------------------------------------------------
+
+#define HID_KBD_MODIFIER_NONE			0x00
+#define HID_KBD_MODIFIER_LEFT_CTRL		0x01
+#define HID_KBD_MODIFIER_LEFT_SHIFT		0x02
+#define HID_KBD_MODIFIER_LEFT_ALT		0x04
+#define HID_KBD_MODIFIER_LEFT_GUI		0x08
+#define HID_KBD_MODIFIER_RIGHT_CTRL		0x10
+#define HID_KBD_MODIFIER_RIGHT_SHIFT	0x20
+#define HID_KBD_MODIFIER_RIGHT_ALT		0x40
+#define HID_KBD_MODIFIER_RIGHT_GUI		0x80
+
+/// HID Keyboard BOOT input report
+struct hid_kbd_bin_report_t
+{
+  uint8_t key_modifier;  	//!< HID_KBD_MODIFIER_xxx Modifiers
+  uint8_t reserved;
+  uint8_t key_code[6];    	//!< key codes
+}__attribute__ ((packed));
+
+#define HID_KBD_LED_NUM_LOCK			0x01
+#define HID_KBD_LED_CAPS_LOCK			0x02
+#define HID_KBD_LED_SCROLL_LOCK			0x04
+#define HID_KBD_LED_COMPOSE				0x08
+#define HID_KBD_LED_KANA				0x10
+
+/// HID Keyboard BOOT input report
+struct hid_kbd_bout_report_t
+{
+  uint8_t key_leds;  	//!< HID_KBD_LED_xxx
+};
+
+#define HID_KBD_SCAN_NONE                0x00 //!< Reserved (no event indicated)
+#define HID_KBD_SCAN_ERRORROLLOVER       0x01 //!< Keyboard ErrorRollOver
+#define HID_KBD_SCAN_POSTFAIL            0x02 //!< Keyboard POSTFail
+#define HID_KBD_SCAN_ERRUNDEF            0x03 //!< Keyboard ErrorUndefined
+#define HID_KBD_SCAN_CAPSLOCK            0x39 //!< CAPSLOCK
+
 
 #endif /* USB_HID_REQS_H_ */
