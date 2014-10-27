ad587c512552cdcf9e31b1b8a338ecdc67ea49db
bratkov <bratkov@netissat.bg>
2014-10-27 12:35:31 +0000

    begin/end wait

--- tmos/services/GUI2/module.mk
+++ tmos/services/GUI2/module.mk
@@ -21,12 +21,12 @@ local_cpp_src-y	+= lcd_SPFD5414D.cpp lcd_ST7565S.cpp lcd_ST7735R.cpp lcd_ST7565S
 local_cpp_src-y += gwindow.cpp gobject.cpp gcontainer.cpp gbutton.cpp gtext.cpp
 local_cpp_src-y += gdialog.cpp gradio.cpp gedit.cpp lcd_multiplex.cpp gfloating_text.cpp
 local_cpp_src-y += gfloating_button.cpp gmenu.cpp gscroll.cpp gmsgbox.cpp
-local_cpp_src-y += glistbox.cpp message.cpp
+local_cpp_src-y += glistbox.cpp message.cpp gdowait.cpp
 
 local_h_src-y 	+= gui_drv.h lcd.h fonts.h stdgui.h lcd_EM6125.h lcd_SPFD5414D.h lcd_ST7735R.h
 local_h_src-y 	+= lcd_ST7565S.h gwindow.h gobject.h gcontainer.h gbutton.h gtext.h message.h lcd_ST7565S_RS.h
 local_h_src-y 	+= gdialog.h gradio.h gedit.h lcd_multiplex.h gfloating_text.h
-local_h_src-y 	+= gfloating_button.h gmenu.h gscroll.h gmsgbox.h glistbox.h
+local_h_src-y 	+= gfloating_button.h gmenu.h gscroll.h gmsgbox.h glistbox.h gdowait.h
 
 #updating global variables
 as_sources 	+= $(call changepath,$(local_as_src-y))
