9f70c7af974faeb541991fd89fdd954eb9394f1b
bratkov@netissat.bg <bratkov@netissat.bg@10a5f5be-b0be-86c2-5f23-71e9d4234c9d>
2014-12-15 11:10:39 +0000

    GTitle and fix text_size calculations

--- trunk/tmos/services/GUI2/gtext.h
+++ trunk/tmos/services/GUI2/gtext.h
@@ -59,5 +59,22 @@ protected:
 	void draw_caption(LCD_MODULE* lcd);
 };
 
+struct GTitle: GText
+{
+	GTitle (GId id_t, const RECT_T& rect_t, CSTRING txt_t, const char* caption_t=NULL,
+			GFlags flags_t = GO_FLG_DEFAULT, unsigned short align_t = SS_DEFAULT, const RENDER_MODE* font_t = &FNT5x7)
+		:GText (id_t, rect_t, txt_t, caption_t,
+				flags_t, align_t, font_t)
+	{;}
+
+	GTitle () :GText()
+	{;}
+
+	virtual ~GTitle()
+	{;}
+
+	virtual unsigned int process_default (GMessage& msg);
+	virtual void draw_this (LCD_MODULE* lcd);
+};
 
 #endif /* GTEXT_H_ */
