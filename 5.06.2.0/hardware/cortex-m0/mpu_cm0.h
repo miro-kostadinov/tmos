3188fa9bc9ae6ac000b927d84ebb541f854421f2
bratkov <bratkov@netissat.bg>
2015-06-22 08:33:34 +0000

    ver 5.06.2.0 a600c

---
+++ 5.06.2.0/hardware/cortex-m0/mpu_cm0.h
@@ -0,0 +1,107 @@
+/*
+ * mpu_cm0.h
+ *
+ *  Created on: Sep 9, 2014
+ *      Author: miro
+ */
+
+#ifndef MPU_CM0_H_
+#define MPU_CM0_H_
+
+#include <tmos_types.h>
+
+/*******************************************************************************
+ *  Memory Protection Unit (MPU) 0xE000ED90
+ ******************************************************************************/
+#ifndef __ASSEMBLY__
+struct MPU_Type
+{
+  __I  uint32_t MPU_TYPE;	//!< (MPU Offset: 0x00) MPU Type Register                              */
+  __IO uint32_t MPU_CTRL;   //!< (MPU Offset: 0x04) MPU Control Register                           */
+  __IO uint32_t MPU_RNR;    //!< (MPU Offset: 0x08) MPU Region Number Register                     */
+  __IO uint32_t MPU_RBAR;   //!< (MPU Offset: 0x0C) MPU Region Base Address Register               */
+  __IO uint32_t MPU_RASR;   //!< (MPU Offset: 0x10) MPU Region Attribute and Size Register         */
+};
+#endif /* __ASSEMBLY__ */
+
+/*******************************************************************************
+ * @defgroup MPU_regs_define
+ * @{
+ ******************************************************************************/
+
+/** @defgroup MPU_TYPE:	   (MPU Offset: 0x00) MPU Type Register                              */
+#define MPU_TYPE_IREGION 		   0x00FF0000 //!< Number of supported MPU instruction regions. Always contains 0x00. The MPU memory map is unified and is described by the DREGION field
+#define MPU_TYPE_DREGION 		   0x0000FF00 //!< Number of supported MPU data regions: 0x08 = eight MPU regions
+#define MPU_TYPE_SEPARATE  		   0x00000001 //!< Indicates support for unified or separate instruction and date memory maps: 0 = unified
+/** @} */
+
+/** @defgroup MPU_CTRL:    (MPU Offset: 0x04) MPU Control Register                           */
+#define MPU_CTRL_PRIVDEFENA 	   0x00000003 //!< Enables privileged software access to the default memory map
+#define MPU_CTRL_HFNMIENA  		   0x00000002 //!< Enables the operation of MPU during hard fault, NMI, and FAULTMASK handlers
+#define MPU_CTRL_ENABLE   		   0x00000001 //!< Enables the MPU
+/** @} */
+
+/** @defgroup MPU_RNR:     (MPU Offset: 0x08) MPU Region Number Register                     */
+#define MPU_RNR_REGION    		   0x000000FF //!< Indicates the MPU region referenced by the MPU_RBAR and MPU_RASR registers
+/** @} */
+
+/** @defgroup MPU_RBAR:    (MPU Offset: 0x0C) MPU Region Base Address Register               */
+#define MPU_RBAR_ADDR     		   0xFFFFFFE0 //!< Region base address
+#define MPU_RBAR_VALID     		   0x00000010 //!< MPU Region Number valid bit
+#define MPU_RBAR_REGION    		   0x0000000F //!< MPU region field
+/** @} */
+
+/** @defgroup MPU_RASR:    (MPU Offset: 0x10) MPU Region Attribute and Size Register         */
+#define MPU_RASR_XN 	   		   0x10000000 //!< Instruction access disable bit
+#define MPU_RASR_AP 	   		   0x07000000 //!< Access permission field
+#define MPU_RASR_AP_PRV_NO_USR_NO  0x00000000 //!<  All accesses generate a permission fault
+#define MPU_RASR_AP_PRV_RW_USR_NO  0x01000000 //!<  Access from privileged software only
+#define MPU_RASR_AP_PRV_RW_USR_RO  0x02000000 //!<  Writes by unprivileged software generate a permission fault
+#define MPU_RASR_AP_PRV_RW_USR_RW  0x03000000 //!<  Full access
+#define MPU_RASR_AP_PRV_RO_USR_NO  0x05000000 //!<  Reads by privileged software only
+#define MPU_RASR_AP_PRV_RO_USR_RO  0x06000000 //!<  Read only, by privileged or unprivileged software
+#define MPU_RASR_TEX 	   		   0x00380000 //!< TEXT Memory access attributes
+#define MPU_RASR_S	 	   		   0x00040000 //!< Shareable bit
+#define MPU_RASR_C	 	   		   0x00020000 //!< C Memory access attributes
+#define MPU_RASR_B	 	   		   0x00010000 //!< B Memory access attributes
+#define MPU_RASR_TEXCB    		   0x003B0000 //!< TEX,C,B Memory access attributes
+#define MPU_RASR_SRD    		   0x0000FF00 //!< Subregion disable bits
+#define MPU_RASR_SIZE     		   0x0000003E //!< Specifies the size of the MPU protection region. Region size in bytes = 2^(SIZE+1)
+#define MPU_RASR_SIZE_32B          0x00000008 //!< 	32B
+#define MPU_RASR_SIZE_64B          0x0000000A //!<  64B
+#define MPU_RASR_SIZE_128B         0x0000000C //!<  128B
+#define MPU_RASR_SIZE_256B         0x0000000E //!<  256B
+#define MPU_RASR_SIZE_512B         0x00000010 //!<  512B
+#define MPU_RASR_SIZE_1K           0x00000012 //!<  1K
+#define MPU_RASR_SIZE_2K           0x00000014 //!<  2K
+#define MPU_RASR_SIZE_4K           0x00000016 //!<  4K
+#define MPU_RASR_SIZE_8K           0x00000018 //!<  8K
+#define MPU_RASR_SIZE_16K          0x0000001A //!<  16K
+#define MPU_RASR_SIZE_32K          0x0000001C //!<  32K
+#define MPU_RASR_SIZE_64K          0x0000001E //!<  64K
+#define MPU_RASR_SIZE_128K         0x00000020 //!<  128K
+#define MPU_RASR_SIZE_256K         0x00000022 //!<  256K
+#define MPU_RASR_SIZE_512K         0x00000024 //!<  512K
+#define MPU_RASR_SIZE_1M           0x00000026 //!<  1M
+#define MPU_RASR_SIZE_2M           0x00000028 //!<  2M
+#define MPU_RASR_SIZE_4M           0x0000002A //!<  4M
+#define MPU_RASR_SIZE_8M           0x0000002C //!<  8M
+#define MPU_RASR_SIZE_16M          0x0000002E //!<  16M
+#define MPU_RASR_SIZE_32M          0x00000030 //!<  32M
+#define MPU_RASR_SIZE_64M          0x00000032 //!<  64M
+#define MPU_RASR_SIZE_128M         0x00000034 //!<  128M
+#define MPU_RASR_SIZE_256M         0x00000036 //!<  256M
+#define MPU_RASR_SIZE_512M         0x00000038 //!<  512M
+#define MPU_RASR_SIZE_1G           0x0000003A //!<  1G
+#define MPU_RASR_SIZE_2G           0x0000003C //!<  2G
+#define MPU_RASR_SIZE_4G           0x0000003E //!<  4G
+#define MPU_RASR_ENABLE    		   0x00000001 //!< Region enable bit
+
+/** @} */
+
+
+/** @} */ // @defgroup MPU_regs_define
+
+
+
+#endif /* MPU_CM0_H_ */
---
+++ 5.06.2.0/ports/cortex-m3/tmos_swi.h
@@ -0,0 +1,288 @@
+//==========================================================================
+// This file is part of TMOS Operating System.
+// Copyright (C) 2007-2008 Informa Ltd.
+//
+// TMOS is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 or (at your option) any later version.
+//
+// TMOS is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+//==========================================================================
+//
+// Author(s):     Miroslav Kostadinov
+// Date:          2008-Apr-03
+// Description:   System functions implemented as SWI
+//
+//========================================================================
+#ifndef TMOS_SWI_H
+#define TMOS_SWI_H
+
+#ifndef TMOS_H
+#error "use tmos.h"
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 System Service Routines
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#ifdef usr_task_suspend_swi
+inline static   void usr_task_suspend(Task* volatile task)
+{
+	register Task *_task asm("r0") = task;
+
+	asm volatile ("swi %1"
+	  : "=r"(_task)
+	  : "I" (usr_task_suspend_swi),  "0" (_task)
+	  : "r1", "r2", "r3", "r12", "r14", "memory");
+}
+#endif
+
+
+#ifdef usr_task_schedule_swi
+inline static   void usr_task_schedule(Task* volatile task)
+{
+	register Task *_task asm("r0") = task;
+
+	asm volatile ("swi %1"
+	  : "=r"(_task)
+	  : "I" (usr_task_schedule_swi),  "0" (_task)
+	  : "r1", "r2", "r3", "r12", "r14", "memory");
+}
+#endif
+
+
+#ifdef tsk_sleep_swi
+inline static   void tsk_sleep( unsigned int volatile time)
+{
+	register unsigned int _time asm("r0") = time;
+
+	asm volatile ("swi %1"
+	  : "=r"(_time)
+	  : "I" (tsk_sleep_swi), "0" (_time)
+	  : "r1", "r2", "r3", "r12", "memory");
+}
+#endif
+
+
+#ifdef tsk_sleep_rel_swi
+inline static   void tsk_sleep_rel( unsigned int volatile time)
+{
+	register unsigned int _time asm("r0") = time;
+
+	asm volatile ("swi %1"
+	  : "=r"(_time)
+	  : "I" (tsk_sleep_rel_swi), "0" (_time)
+	  : "r1", "r2", "r3", "r12", "memory");
+}
+#endif
+
+
+#ifdef tsk_sleep_until_swi
+inline static   void tsk_sleep_until( unsigned int volatile time)
+{
+	register unsigned int _time asm("r0") = time;
+
+	asm volatile ("swi %1"
+	  : "=r"(_time)
+	  : "I" (tsk_sleep_until_swi), "0" (_time)
+	  : "r1", "r2", "r3", "r12", "memory");
+}
+#endif
+
+
+#ifdef sys_increment_ticks_swi
+inline static   void sys_increment_ticks(unsigned int volatile time)
+{
+	register unsigned int _time asm("r0") = time;
+
+	asm volatile ("swi %1"
+	  : "=r"(_time)
+	  : "I" (sys_increment_ticks_swi), "0" (_time)
+	  : "r1", "r2", "r3", "r12", "r14", "memory");
+}
+#endif
+
+
+#ifdef usr_send_signal_swi
+inline static   void usr_send_signal(Task* volatile task, unsigned int volatile signal)
+{
+	register Task *_task asm("r0") = task;
+	register unsigned int _signal asm("r1") = signal;
+
+	asm volatile ("swi %2"
+	  : "=r" (_task), "=r"(_signal)
+	  : "I" (usr_send_signal_swi), "0" (_task), "1"(_signal)
+	  : "r2", "r3", "r12", "r14", "memory");
+}
+inline static   void tsk_send_signal(Task* volatile task, unsigned int volatile signal)
+{
+	register Task *_task asm("r0") = task;
+	register unsigned int _signal asm("r1") = signal;
+
+	asm volatile ("swi %2"
+	  :  "=r" (_task), "=r"(_signal)
+	  : "I" (usr_send_signal_swi), "0" (_task), "1"(_signal)
+	  : "r2", "r3", "r12", "memory");
+}
+#endif
+
+
+#ifdef tsk_try_signal_swi
+inline static   unsigned int tsk_try_signal(unsigned int volatile signal)
+{
+	register unsigned int _signal asm("r0") = signal;
+
+	asm volatile ("swi %1"
+	  : "=r"(_signal)
+	  : "I" (tsk_try_signal_swi), "0"(_signal)
+	  : "r1", "r2", "r3", "r12", "memory");
+	return (_signal);
+}
+#endif
+
+
+#ifdef tsk_get_signal_swi
+inline static   unsigned int tsk_get_signal(unsigned int volatile signal)
+{
+	register unsigned int _signal asm("r0") = signal;
+
+	asm volatile ("swi %1"
+	  : "=r"(_signal)
+	  : "I" (tsk_get_signal_swi), "0"(_signal)
+	  : "r1", "r2", "r3", "r12", "memory");
+	return (_signal);
+}
+#endif
+
+
+#ifdef tsk_wait_signal_swi
+inline static   unsigned int tsk_wait_signal(unsigned int volatile signal, unsigned int volatile time)
+{
+	register unsigned int _signal asm("r0") = signal;
+	register unsigned int _time asm("r1") = time;
+
+	asm volatile ("swi %2"
+	  : "=r"(_signal), "=r"(_time)
+	  : "I" (tsk_wait_signal_swi), "0"(_signal), "1"(_time)
+	  : "r2", "r3", "r12", "memory");
+	return (_signal);
+}
+#endif
+
+
+#ifdef tsk_resume_wait_signal_swi
+inline static   unsigned int tsk_resume_wait_signal(unsigned int volatile signal)
+{
+	register unsigned int _signal asm("r0") = signal;
+
+	asm volatile ("swi %1"
+	  : "=r"(_signal)
+	  : "I" (tsk_resume_wait_signal_swi), "0"(_signal)
+	  : "r1", "r2", "r3", "r12", "memory");
+	return (_signal);
+}
+#endif
+
+
+#ifdef usr_drv_icontrol_swi
+inline static   void usr_drv_icontrol(DRIVER_INDEX volatile drv_index, unsigned int volatile reason, void * volatile param)
+{
+	register unsigned int _drv_index asm("r0") = drv_index;
+	register unsigned int _reason asm("r1") = reason;
+	register void * _param asm("r2") = param;
+
+	asm volatile ("swi %0"
+	  :
+	  : "I" (usr_drv_icontrol_swi), "r"(_drv_index), "r"(_reason), "r"(_param)
+	  : "r3", "memory");
+}
+#define tsk_drv_icontrol(args...) usr_drv_icontrol(args)
+#endif
+
+
+#ifdef isr_contol_swi
+typedef const struct DRIVER_INFO_Type * DRIVER_INFO;
+inline static void isr_contol(DRIVER_INFO volatile drv_info, unsigned int volatile reason, void* volatile param)
+{
+	register DRIVER_INFO _drv_info asm("r0") = drv_info;
+	register unsigned int _reason asm("r1") = reason;
+	register void * _param asm("r2") = param;
+
+	asm volatile ("swi %0"
+	  :
+	  : "I" (isr_contol_swi), "r"(_drv_info), "r"(_reason), "r"(_param)
+	  : "r3", "memory");
+}
+#endif
+
+
+
+
+
+#ifdef malloc_swi
+inline static void* usr_malloc(unsigned int volatile size)
+{
+	register unsigned int _size asm("r0") = size;
+
+	asm volatile ("swi %1"
+	  : "=r"(_size)
+	  : "I" (malloc_swi), "0"(_size)
+	  : "r3", "memory");
+	return ((void*)_size);
+}
+#endif
+
+#ifdef free_swi
+inline static void usr_free(void* volatile ptr)
+{
+	register void* _ptr asm("r0") = ptr;
+
+	asm volatile ("swi %1"
+	  : "=r"(_ptr)
+	  : "I" (free_swi), "0"(_ptr)
+	  : "r3", "memory");
+}
+#endif
+
+#ifdef realloc_swi
+inline static void* usr_realloc(void* volatile ptr, unsigned int volatile size)
+{
+	register void* _ptr asm("r0") = ptr;
+	register unsigned int _size asm("r1") = size;
+
+	asm volatile ("swi %1"
+	  : "=r"(_ptr)
+	  : "I" (realloc_swi), "0"(_ptr), "r"(_size)
+	  : "r3", "memory");
+	return ((void*)_ptr);
+}
+#endif
+
+#if USE_CPU_SLEEP_MODE
+inline static void usr_sleep_on_exit(void)
+{
+	asm volatile ("swi %0"
+	  :
+	  : "I" (wfi_swi)
+	  : "r3", "memory");
+}
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+
