1b33cb598c2671d0cfede0f21710d31f0deaade2
miro-kostadinov <miro.kostadinov@gmail.com>
2016-01-26 15:00:27 +0200

    f0m1 board added

---
+++ tmos/boards/f0m1/brd_cpp.h
@@ -0,0 +1,70 @@
+/*
+ *
+ *  This file should provide all core and mcu specific defines and types.
+ *
+ *  C usage:
+ *  	From application level is preferred to include <drivers.h>
+ *  	<drivers.h> will include <mcu.h>
+ *
+ */
+#ifndef BRD_CPP_H_
+#define BRD_CPP_H_
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 		 defines first
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#ifdef __cplusplus
+ extern "C" {
+
+enum DRIVER_INDEX : signed char
+{
+ /******  Cortex-M0 Processor Exceptions Numbers **************************************************************/
+	NonMaskableInt_IRQn         = -14,    /*!< 2 Non Maskable Interrupt                                        */
+	HardFault_IRQn              = -13,    /*!< 3 Cortex-M0 Hard Fault Interrupt                                */
+	SVC_IRQn                    = -5,     /*!< 11 Cortex-M0 SV Call Interrupt                                  */
+	PendSV_IRQn                 = -2,     /*!< 14 Cortex-M0 Pend SV Interrupt                                  */
+	SysTick_IRQn                = -1,     /*!< 15 Cortex-M0 System Tick Interrupt                              */
+
+ /******  STM32F030x8 specific Interrupt Numbers **************************************************************/
+	WWDG_IRQn                   = 0,      /*!< Window WatchDog Interrupt                                       */
+	RTC_IRQn                    = 2,      /*!< RTC Interrupt through EXTI Lines 17, 19 and 20                  */
+	FLASH_IRQn                  = 3,      /*!< FLASH global Interrupt                                          */
+	RCC_IRQn                    = 4,      /*!< RCC global Interrupt                                            */
+	EXTI0_1_IRQn                = 5,      /*!< EXTI Line 0 and 1 Interrupts                                    */
+	EXTI2_3_IRQn                = 6,      /*!< EXTI Line 2 and 3 Interrupts                                    */
+	EXTI4_15_IRQn               = 7,      /*!< EXTI Line 4 to 15 Interrupts                                    */
+	DMA1_Channel1_IRQn          = 9,      /*!< DMA1 Channel 1 Interrupt                                        */
+	DMA1_Channel2_3_IRQn        = 10,     /*!< DMA1 Channel 2 and Channel 3 Interrupts                         */
+	DMA1_Channel4_5_IRQn        = 11,     /*!< DMA1 Channel 4 and Channel 5 Interrupts                         */
+	ADC1_IRQn                   = 12,     /*!< ADC1 global Interrupt                                           */
+	TIM1_BRK_UP_TRG_COM_IRQn    = 13,     /*!< TIM1 Break, Update, Trigger and Commutation Interrupts          */
+	TIM1_CC_IRQn                = 14,     /*!< TIM1 Capture Compare Interrupt                                  */
+	TIM3_IRQn                   = 16,     /*!< TIM3 global Interrupt                                           */
+	TIM6_IRQn                   = 17,     /*!< TIM6 global Interrupt                                           */
+	TIM14_IRQn                  = 19,     /*!< TIM14 global Interrupt                                          */
+	TIM15_IRQn                  = 20,     /*!< TIM15 global Interrupt                                          */
+	TIM16_IRQn                  = 21,     /*!< TIM16 global Interrupt                                          */
+	TIM17_IRQn                  = 22,     /*!< TIM17 global Interrupt                                          */
+	I2C1_IRQn                   = 23,     /*!< I2C1 Event Interrupt                                            */
+	I2C2_IRQn                   = 24,     /*!< I2C2 Event Interrupt                                            */
+	SPI1_IRQn                   = 25,     /*!< SPI1 global Interrupt                                           */
+	SPI2_IRQn                   = 26,     /*!< SPI2 global Interrupt                                           */
+	USART1_IRQn                 = 27,     /*!< USART1 global Interrupt                                         */
+	USART2_IRQn                 = 28,     /*!< USART2 global Interrupt                                         */
+	USART3_4_5_6_IRQn           = 29,     /*!< USART2-3-4-5-6 global Interrupt                                         */
+	INALID_DRV_INDX	                /*!<  invalid	                         */
+};
+
+}
+
+#else
+
+typedef signed char DRIVER_INDEX;
+
+#endif
+
+
+
+
+#endif /*BRD_CPP_H_*/
---
+++ tmos/boards/f0m1/module.mk
@@ -0,0 +1,127 @@
+#-------------------------------------------------------------------------------
+#
+#	Config file for f0m1
+#	
+#   www.thumbos.com
+#
+#-------------------------------------------------------------------------------
+
+OUT_NAME	?= f0m1
+OUT_DIR		?= out/$(OUT_NAME)/
+
+#  optimisation level  can be [0, 1, 2, 3, s].
+OPT			?= -Os
+
+# Build library/binary/hex. can be [y, n]
+BUILD_LIB	?= y
+BUILD_BIN	?= n
+BUILD_HEX	?= n
+
+# Hardware selection
+CFG_CORE 	?= cortex-m0
+CFG_FAMILY	?= stm32
+CFG_SERIES	?= stm32f0
+CFG_MCU 	?= STM32F030CC
+
+
+# Debugging format.
+DEBUG 		?= dwarf-2
+
+
+#=========================   Project settings       ===========================#
+
+# Select Target CPU
+
+
+
+#========================    TMOS settings		===============================#
+
+# memcmp() memset() memclr() memcpy() memmove() strcpy() strncpy() strlen()
+# strchr() strrchr() strpbrk() islower() isupper() strcmp() strncmp() strcasecmp()
+# strncasecmp() toupper() tolower() XNUM_TO_DIGIT()
+USE_TMOS_STRING	?=y
+
+# TMOS_TL_V2
+USE_TMOS_TL_V2	?=y
+USE_CSTRING		?=y
+
+# tmos stdlib -> sys_malloc() sys_realloc() svc_free()
+USE_TMOS_STDLIB	?=y
+
+# class CTime
+USE_TIME		?=n
+
+# sprintf() scanf() 
+USE_TMOS_STDIO	?=y
+
+# use tmos long long operations
+USE_TMOS_LLONG	?=n
+
+#CalculateCRC32()
+#CalculateCRC32()
+USE_CRC16		?=n
+USE_CRC32		?=n
+
+# class CCache
+USE_CACHE		?=n
+
+# SHA-1 class
+USE_SHA_1		?=n
+
+# base 32 
+USE_BASE32		?=n
+
+# GUI
+USE_GUI			?=n
+
+# USB
+USE_USB			?=n
+
+# AES TMOS
+USE_AES_TMOS	?=n
+
+# GPIO output expander (shift register)
+USE_GPIO_EXPANDER ?=n
+
+#net
+USE_NET			?=y
+
+
+################################################################################
+
+# local sources
+local_as_src-y	:=
+local_c_src-y 	:=
+local_cpp_src-y	:=
+local_h_src-y 	:=
+
+local_c_src-y 	+= swi_tab.c
+ 
+local_h_src-y 	+= brd_cfg.h brd_cpp.h swi_tab.h trace.h
+
+#updating global variables
+as_sources 	+= $(call changepath,$(local_as_src-y))
+c_sources  	+= $(call changepath,$(local_c_src-y))
+cpp_sources += $(call changepath,$(local_cpp_src-y))
+h_sources  	+= $(call changepath,$(local_h_src-y))
+
+#add current directory to include path
+inc_dirs += $(subdirectory) 
+
+#submodules
+local_modules-y := 
+
+sub_modules := $(call changepath,$(local_modules-y))
+modules += $(sub_modules)
+
+include $(addsuffix /module.mk,$(sub_modules))
+
+#========================     Versioning          =============================#
+DEP_PROJECTS := 
+TARGET_VER_FILE := $(subdirectory)versions.h 
+
+ifneq "$(MAKECMDGOALS)" "clean"
+ifneq "$(MAKECMDGOALS)" "all"
+include boards/release.mk
+endif
+endif
