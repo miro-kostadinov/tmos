855517d1735391005bce7c03c509bb69a70c3f74
bratkov <s.bratkov@eltrade.com>
2017-01-12 11:20:00 +0200

    target revolution

--- tmos/boards/a600c/brd_cfg.h
+++ tmos/boards/a600c/brd_cfg.h
@@ -19,55 +19,6 @@
 #define KEY_PRESREP_MASK	0x7FFF
 #define KEY_STATE_MASK		(KEY_REPEAT_CODE|KEY_UP_CODE)
 
-/*
-#define KEY_ON				0x00         //KEY_ON
-#define KEY_D_PLUS			0x01         //KEY_AMT
-#define KEY_HASHTAG			0x02         //KEY_RA
-#define KEY_STAR			0x03         //KEY_RFN
-//#define KEY_SHIFT			0x04         //KEY_HASHTAG
-#define KEY_TOTAL			0x05         //KEY_TOTAL
-#define KEY_PLU				0x06         //KEY_PLU
-#define KEY_RFN				0x07         //KEY_PO
-#define KEY_PRC_PLUS		0x08         //KEY_PRC_PLUS
-#define KEY_PRC_MINUS		0x09         //KEY_PRC_MINUS
-#define KEY_STL				0x0A         //KEY_STL
-#define KEY_CL				0x0B         //KEY_CL
-//#define KEY_7				0x0C         //KEY_7
-//#define KEY_4				0x0D         //KEY_4
-//#define KEY_1				0x0E         //KEY_1
-//#define KEY_X				0x0F         //KEY_0
-#define KEY_VD				0x10         //KEY_VD
-//#define KEY_8				0x11         //KEY_8
-//#define KEY_5				0x12         //KEY_5
-//#define KEY_2				0x13         //KEY_2
-//#define KEY_0				0x14         //KEY_00
-#define KEY_AMT				0x15         //KEY_X
-//#define KEY_9				0x16         //KEY_9
-//#define KEY_6				0x17         //KEY_6
-//#define KEY_3				0x18         //KEY_3
-//#define KEY_DP				0x19         //KEY_DP
-#define KEY_PF				0x1A         //KEY_PF
-#define KEY_DEP_4			0x1B         //KEY_DEP_4
-#define KEY_DEP_3			0x1C         //KEY_DEP_3
-#define KEY_DEP_2			0x1D         //KEY_DEP_2
-#define KEY_DEP_1			0x1E         //KEY_DEP_1
-#define KEY_NC				0x1F
-#define KEY_PY2				0x20         //KEY_CARD
-#define KEY_PY1				0x21         //KEY_CHECK
-#define KEY_DEP_6			0x22         //KEY_SHIFT
-#define KEY_DEP_5			0x23         //KEY_DEP_5
-*/
-/*
-#define KEY_OK				KEY_TOTAL
-#define KEY_ENTER			KEY_TOTAL
-#define KEY_CANCEL			KEY_CL
-#define KEY_C				KEY_CL
-#define KEY_UP				KEY_DEP_3
-#define KEY_DOWN			KEY_DEP_2
-#define KEY_LEFT			KEY_STL
-#define KEY_RIGHT			KEY_DEP_5
-#define KEY_ESC				KEY_ON
-*/
 
 #define USE_TMOS_TL_V2		1
 #define USE_CSTRING			1
@@ -119,5 +70,17 @@
 
 /// Ethernet
 #define USE_LWIP_1_4_1		1
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		USART DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#define USART_DRV_RX_BUF_SIZE	(1024)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		Wi-Fi ESP8266 DRIVER
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#define USE_WIFI 			1
+#define USB_WIFI_ESP8266	1
+
 
 #endif /* BRD_CFG_H_ */
