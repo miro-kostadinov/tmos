0b2eec56d5b43abb5e50c2a9c49f79d625b563b8
bratkov <s.bratkov@eltrade.com>
2018-02-27 14:52:27 +0200

    ES_NUMERIC only digits

--- tmos/services/GUI2/stdgui.h
+++ tmos/services/GUI2/stdgui.h
@@ -174,8 +174,9 @@ typedef unsigned char GId;
 #define ES_PASSWORD		(1<<9)
 #define ES_WANTRETURN   (1<<10)
 #define ES_HIDE_CURSOR	(1<<11)
-#define ES_AUTO_SCROLL	(1<<12)
+#define ES_AUTO_SCROLL	SS_AUTO_SCROLL
 #define ES_LEAD_ZERO	(1<<14)
+#define ES_NUMERIC		(1<<15)
 
 #define ES_DEFAULT		(ES_CENTER|ES_MIDDLE|ES_MULTILINE|ES_AUTO_SCROLL)
 
