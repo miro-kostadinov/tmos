a895e9d332154741e436b1275fa5e069545db1c9
miro-kostadinov <miro.kostadinov@gmail.com>
2019-01-04 14:39:56 +0200

    md5 bugs

--- tmos/services/security/crypto/md5.cpp
+++ tmos/services/security/crypto/md5.cpp
@@ -65,14 +65,10 @@ void md5_algo_t::Result(void* digest)
 		buf8[indx++] = 0;
 	}
 
-	buf32[14] = __REV(total_bits);
-	buf32[15] = __REV(total_bits >> 32);
+	buf32[14] = total_bits;
+	buf32[15] = total_bits >> 32;
 
 	process_block(buf32);
-
-	//Convert from host byte order to big-endian byte order
-	for (uint32_t i = 0; i < 5; i++)
-		h[i] = __REV(h[i]);
 
 	//Copy the resulting digest
 	if (digest)
@@ -128,12 +124,8 @@ void md5_algo_t::Result(void* digest)
 
 void md5_algo_t::process_block(const uint32_t* block)
 {
-	uint32_t i, a, b, c, d;
+	uint32_t a, b, c, d;
 	uint32_t* h = digest32();
-
-	//Convert from big-endian byte order to host byte order
-	for (i = 0; i < 16; i++)
-		buf32[i] = __REV(block[i]);
 
 	//Initialize the 5 working registers
 	a = h[0];
@@ -143,76 +135,76 @@ void md5_algo_t::process_block(const uint32_t* block)
 
 
     /* Round 1 */
-    FF (a, b, c, d, buf32[ 0], S11, 0xd76aa478); /* 1 */
-    FF (d, a, b, c, buf32[ 1], S12, 0xe8c7b756); /* 2 */
-    FF (c, d, a, b, buf32[ 2], S13, 0x242070db); /* 3 */
-    FF (b, c, d, a, buf32[ 3], S14, 0xc1bdceee); /* 4 */
-    FF (a, b, c, d, buf32[ 4], S11, 0xf57c0faf); /* 5 */
-    FF (d, a, b, c, buf32[ 5], S12, 0x4787c62a); /* 6 */
-    FF (c, d, a, b, buf32[ 6], S13, 0xa8304613); /* 7 */
-    FF (b, c, d, a, buf32[ 7], S14, 0xfd469501); /* 8 */
-    FF (a, b, c, d, buf32[ 8], S11, 0x698098d8); /* 9 */
-    FF (d, a, b, c, buf32[ 9], S12, 0x8b44f7af); /* 10 */
-    FF (c, d, a, b, buf32[10], S13, 0xffff5bb1); /* 11 */
-    FF (b, c, d, a, buf32[11], S14, 0x895cd7be); /* 12 */
-    FF (a, b, c, d, buf32[12], S11, 0x6b901122); /* 13 */
-    FF (d, a, b, c, buf32[13], S12, 0xfd987193); /* 14 */
-    FF (c, d, a, b, buf32[14], S13, 0xa679438e); /* 15 */
-    FF (b, c, d, a, buf32[15], S14, 0x49b40821); /* 16 */
+    FF (a, b, c, d, block[ 0], S11, 0xd76aa478); /* 1 */
+    FF (d, a, b, c, block[ 1], S12, 0xe8c7b756); /* 2 */
+    FF (c, d, a, b, block[ 2], S13, 0x242070db); /* 3 */
+    FF (b, c, d, a, block[ 3], S14, 0xc1bdceee); /* 4 */
+    FF (a, b, c, d, block[ 4], S11, 0xf57c0faf); /* 5 */
+    FF (d, a, b, c, block[ 5], S12, 0x4787c62a); /* 6 */
+    FF (c, d, a, b, block[ 6], S13, 0xa8304613); /* 7 */
+    FF (b, c, d, a, block[ 7], S14, 0xfd469501); /* 8 */
+    FF (a, b, c, d, block[ 8], S11, 0x698098d8); /* 9 */
+    FF (d, a, b, c, block[ 9], S12, 0x8b44f7af); /* 10 */
+    FF (c, d, a, b, block[10], S13, 0xffff5bb1); /* 11 */
+    FF (b, c, d, a, block[11], S14, 0x895cd7be); /* 12 */
+    FF (a, b, c, d, block[12], S11, 0x6b901122); /* 13 */
+    FF (d, a, b, c, block[13], S12, 0xfd987193); /* 14 */
+    FF (c, d, a, b, block[14], S13, 0xa679438e); /* 15 */
+    FF (b, c, d, a, block[15], S14, 0x49b40821); /* 16 */
 
     /* Round 2 */
-    GG (a, b, c, d, buf32[ 1], S21, 0xf61e2562); /* 17 */
-    GG (d, a, b, c, buf32[ 6], S22, 0xc040b340); /* 18 */
-    GG (c, d, a, b, buf32[11], S23, 0x265e5a51); /* 19 */
-    GG (b, c, d, a, buf32[ 0], S24, 0xe9b6c7aa); /* 20 */
-    GG (a, b, c, d, buf32[ 5], S21, 0xd62f105d); /* 21 */
-    GG (d, a, b, c, buf32[10], S22, 0x02441453); /* 22 */
-    GG (c, d, a, b, buf32[15], S23, 0xd8a1e681); /* 23 */
-    GG (b, c, d, a, buf32[ 4], S24, 0xe7d3fbc8); /* 24 */
-    GG (a, b, c, d, buf32[ 9], S21, 0x21e1cde6); /* 25 */
-    GG (d, a, b, c, buf32[14], S22, 0xc33707d6); /* 26 */
-    GG (c, d, a, b, buf32[ 3], S23, 0xf4d50d87); /* 27 */
-    GG (b, c, d, a, buf32[ 8], S24, 0x455a14ed); /* 28 */
-    GG (a, b, c, d, buf32[13], S21, 0xa9e3e905); /* 29 */
-    GG (d, a, b, c, buf32[ 2], S22, 0xfcefa3f8); /* 30 */
-    GG (c, d, a, b, buf32[ 7], S23, 0x676f02d9); /* 31 */
-    GG (b, c, d, a, buf32[12], S24, 0x8d2a4c8a); /* 32 */
+    GG (a, b, c, d, block[ 1], S21, 0xf61e2562); /* 17 */
+    GG (d, a, b, c, block[ 6], S22, 0xc040b340); /* 18 */
+    GG (c, d, a, b, block[11], S23, 0x265e5a51); /* 19 */
+    GG (b, c, d, a, block[ 0], S24, 0xe9b6c7aa); /* 20 */
+    GG (a, b, c, d, block[ 5], S21, 0xd62f105d); /* 21 */
+    GG (d, a, b, c, block[10], S22, 0x02441453); /* 22 */
+    GG (c, d, a, b, block[15], S23, 0xd8a1e681); /* 23 */
+    GG (b, c, d, a, block[ 4], S24, 0xe7d3fbc8); /* 24 */
+    GG (a, b, c, d, block[ 9], S21, 0x21e1cde6); /* 25 */
+    GG (d, a, b, c, block[14], S22, 0xc33707d6); /* 26 */
+    GG (c, d, a, b, block[ 3], S23, 0xf4d50d87); /* 27 */
+    GG (b, c, d, a, block[ 8], S24, 0x455a14ed); /* 28 */
+    GG (a, b, c, d, block[13], S21, 0xa9e3e905); /* 29 */
+    GG (d, a, b, c, block[ 2], S22, 0xfcefa3f8); /* 30 */
+    GG (c, d, a, b, block[ 7], S23, 0x676f02d9); /* 31 */
+    GG (b, c, d, a, block[12], S24, 0x8d2a4c8a); /* 32 */
 
     /* Round 3 */
-    HH (a, b, c, d, buf32[ 5], S31, 0xfffa3942); /* 33 */
-    HH (d, a, b, c, buf32[ 8], S32, 0x8771f681); /* 34 */
-    HH (c, d, a, b, buf32[11], S33, 0x6d9d6122); /* 35 */
-    HH (b, c, d, a, buf32[14], S34, 0xfde5380c); /* 36 */
-    HH (a, b, c, d, buf32[ 1], S31, 0xa4beea44); /* 37 */
-    HH (d, a, b, c, buf32[ 4], S32, 0x4bdecfa9); /* 38 */
-    HH (c, d, a, b, buf32[ 7], S33, 0xf6bb4b60); /* 39 */
-    HH (b, c, d, a, buf32[10], S34, 0xbebfbc70); /* 40 */
-    HH (a, b, c, d, buf32[13], S31, 0x289b7ec6); /* 41 */
-    HH (d, a, b, c, buf32[ 0], S32, 0xeaa127fa); /* 42 */
-    HH (c, d, a, b, buf32[ 3], S33, 0xd4ef3085); /* 43 */
-    HH (b, c, d, a, buf32[ 6], S34, 0x04881d05); /* 44 */
-    HH (a, b, c, d, buf32[ 9], S31, 0xd9d4d039); /* 45 */
-    HH (d, a, b, c, buf32[12], S32, 0xe6db99e5); /* 46 */
-    HH (c, d, a, b, buf32[15], S33, 0x1fa27cf8); /* 47 */
-    HH (b, c, d, a, buf32[ 2], S34, 0xc4ac5665); /* 48 */
+    HH (a, b, c, d, block[ 5], S31, 0xfffa3942); /* 33 */
+    HH (d, a, b, c, block[ 8], S32, 0x8771f681); /* 34 */
+    HH (c, d, a, b, block[11], S33, 0x6d9d6122); /* 35 */
+    HH (b, c, d, a, block[14], S34, 0xfde5380c); /* 36 */
+    HH (a, b, c, d, block[ 1], S31, 0xa4beea44); /* 37 */
+    HH (d, a, b, c, block[ 4], S32, 0x4bdecfa9); /* 38 */
+    HH (c, d, a, b, block[ 7], S33, 0xf6bb4b60); /* 39 */
+    HH (b, c, d, a, block[10], S34, 0xbebfbc70); /* 40 */
+    HH (a, b, c, d, block[13], S31, 0x289b7ec6); /* 41 */
+    HH (d, a, b, c, block[ 0], S32, 0xeaa127fa); /* 42 */
+    HH (c, d, a, b, block[ 3], S33, 0xd4ef3085); /* 43 */
+    HH (b, c, d, a, block[ 6], S34, 0x04881d05); /* 44 */
+    HH (a, b, c, d, block[ 9], S31, 0xd9d4d039); /* 45 */
+    HH (d, a, b, c, block[12], S32, 0xe6db99e5); /* 46 */
+    HH (c, d, a, b, block[15], S33, 0x1fa27cf8); /* 47 */
+    HH (b, c, d, a, block[ 2], S34, 0xc4ac5665); /* 48 */
 
     /* Round 4 */
-    II (a, b, c, d, buf32[ 0], S41, 0xf4292244); /* 49 */
-    II (d, a, b, c, buf32[ 7], S42, 0x432aff97); /* 50 */
-    II (c, d, a, b, buf32[14], S43, 0xab9423a7); /* 51 */
-    II (b, c, d, a, buf32[ 5], S44, 0xfc93a039); /* 52 */
-    II (a, b, c, d, buf32[12], S41, 0x655b59c3); /* 53 */
-    II (d, a, b, c, buf32[ 3], S42, 0x8f0ccc92); /* 54 */
-    II (c, d, a, b, buf32[10], S43, 0xffeff47d); /* 55 */
-    II (b, c, d, a, buf32[ 1], S44, 0x85845dd1); /* 56 */
-    II (a, b, c, d, buf32[ 8], S41, 0x6fa87e4f); /* 57 */
-    II (d, a, b, c, buf32[15], S42, 0xfe2ce6e0); /* 58 */
-    II (c, d, a, b, buf32[ 6], S43, 0xa3014314); /* 59 */
-    II (b, c, d, a, buf32[13], S44, 0x4e0811a1); /* 60 */
-    II (a, b, c, d, buf32[ 4], S41, 0xf7537e82); /* 61 */
-    II (d, a, b, c, buf32[11], S42, 0xbd3af235); /* 62 */
-    II (c, d, a, b, buf32[ 2], S43, 0x2ad7d2bb); /* 63 */
-    II (b, c, d, a, buf32[ 9], S44, 0xeb86d391); /* 64 */
+    II (a, b, c, d, block[ 0], S41, 0xf4292244); /* 49 */
+    II (d, a, b, c, block[ 7], S42, 0x432aff97); /* 50 */
+    II (c, d, a, b, block[14], S43, 0xab9423a7); /* 51 */
+    II (b, c, d, a, block[ 5], S44, 0xfc93a039); /* 52 */
+    II (a, b, c, d, block[12], S41, 0x655b59c3); /* 53 */
+    II (d, a, b, c, block[ 3], S42, 0x8f0ccc92); /* 54 */
+    II (c, d, a, b, block[10], S43, 0xffeff47d); /* 55 */
+    II (b, c, d, a, block[ 1], S44, 0x85845dd1); /* 56 */
+    II (a, b, c, d, block[ 8], S41, 0x6fa87e4f); /* 57 */
+    II (d, a, b, c, block[15], S42, 0xfe2ce6e0); /* 58 */
+    II (c, d, a, b, block[ 6], S43, 0xa3014314); /* 59 */
+    II (b, c, d, a, block[13], S44, 0x4e0811a1); /* 60 */
+    II (a, b, c, d, block[ 4], S41, 0xf7537e82); /* 61 */
+    II (d, a, b, c, block[11], S42, 0xbd3af235); /* 62 */
+    II (c, d, a, b, block[ 2], S43, 0x2ad7d2bb); /* 63 */
+    II (b, c, d, a, block[ 9], S44, 0xeb86d391); /* 64 */
 
 
 	//Update the hash value
