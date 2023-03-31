1eedc970b14abe758eb121fc8a8a5145e3195c62
bratkov <s.bratkov@eltrade.com>
2023-03-31 16:36:29 +0300

    traces

--- tmos/services/GUI2/stdgui.cpp
+++ tmos/services/GUI2/stdgui.cpp
@@ -11,13 +11,22 @@
 
 
 unsigned int current_laguage;
-#if GUI_DEBUG_MESSAGES || GUI_DEBUG
+
+#if GUI_DEBUG_MESSAGES || GUI_DEBUG || GUI_DEBUG_INVALIDATE
 STR_LIST obj_type_str =
 		SZ(OBJECT_OBJECT) SZ(OBJECT_CONTAINER) SZ(OBJECT_BUTTON) SZ(OBJECT_EDIT)
 		SZ(OBJECT_FBUTTON) SZ(OBJECT_FTEXT) SZ(OBJECT_LISTBOX) SZ(OBJECT_MENU)
 		SZ(OBJECT_RADIO) SZ(OBJECT_TEXT) SZ(OBJECT_WINDOW) SZ(OBJECT_DIALOG)
 		SZ(OBJECT_MESSAGEBOX) SZ(OBJECT_DOWAIT) SZ(OBJECT_DISPLAY)
-		SZ(OBJECT_DISPLAY_MULTIPLEXER);
+		SZ(OBJECT_DISPLAY_MULTIPLEXER)
+		SZ(OBJECT_CPU_USAGE);
+
+void gui_trace_sleep()
+{
+	uint32_t mytime = CURRENT_TASK->time;
+	tsk_sleep(5);
+	CURRENT_TASK->time = mytime;
+}
 #endif
 
 #if GUI_DEBUG_MESSAGES
@@ -25,20 +34,19 @@ STR_LIST wm_dbg_str = SZ(WM_DELETED) SZ(WM_QUIT) SZ(WN_DESTROY) SZ(WM_CLOSE)
 		SZ(WM_COMMAND) SZ(WM_CHANGE) SZ(WM_IDLE) SZ(WM_SET_FLAGS)
 		SZ(WM_CLR_FLAGS) SZ(WM_SETFOCUS) SZ(WM_KILLFOCUS) SZ(WM_TIMER) SZ(WM_INIT) SZ(WM_DRAW) SZ(WM_KEY);
 
-void trace_message(const GMessage& msg)
+void GDebug_trace_message(const GMessage& msg, bool push)
 {
 	if(msg.code == WM_IDLE)
 		return;
-
-	TRACELN1("\e[4;1;32m");
-	if(msg.dst && msg.code != WM_DELETED)
-	{
-		TRACE("%X[%d]%s ( %s 0x%X/%d\e[m", msg.dst, msg.dst->id,
-				szlist_at(obj_type_str, msg.dst->get_object_type()),
-				szlist_at(wm_dbg_str, msg.code), msg.param, msg.param);
-	}
+	if(push)
+		TRACELN1("\e[1;93m< "); // yellow
 	else
-		TRACE("%X[INVALID] ( %s 0x%X/%d\e[m", msg.dst, szlist_at(wm_dbg_str, msg.code), msg.param, msg.param);
+		TRACELN1("\e[1;92m> ");// green
+
+	TRACE("%X[%d]%s: %s,%d", msg.dst, msg.dst->id,
+			szlist_at(wm_dbg_str, msg.code),
+			szlist_at(obj_type_str, msg.dst->get_object_type()),
+			msg.param);
 	if(msg.code == WM_DRAW)
 	{
 		if(msg.lparam)
@@ -48,7 +56,42 @@ void trace_message(const GMessage& msg)
 	}
 	else
 		TRACE(" %lX ", msg.lparam);
-	TRACE1("\e[4;1;32m)\e[m");
+	TRACE1("\e[m");
+	gui_trace_sleep();
+}
+#endif
+
+#if GUI_DEBUG_INVALIDATE
+static unsigned int gui_trace_level;
+
+static void trace_line(const char*line, const RECT_T& rect, GObject* obj)
+{
+	TRACE1("\r\n* ");
+	for(unsigned int i=0; i<gui_trace_level; i++)
+	{
+		TRACE1(" ");
+	}
+	TRACE1(line);
+	if(obj)
+	{
+		TRACE_CHAR(':');
+		TRACE1(szlist_at(obj_type_str, obj->get_object_type()));
+	}
+	rect.dump();
+	gui_trace_sleep();
+}
+
+void trace_enter(const char*line, const RECT_T& rect, GObject* obj)
+{
+	trace_line(line, rect, obj);
+	gui_trace_level++;
+}
+
+void trace_leave(const char*line, const RECT_T& rect, GObject* obj)
+{
+	if(gui_trace_level)
+		gui_trace_level--;
+	trace_line(line, rect, obj);
 }
 #endif
 
