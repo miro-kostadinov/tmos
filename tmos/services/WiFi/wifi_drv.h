ceda20bb40f2d753efd829f0177f2c7313552bc0
bratkov <s.bratkov@eltrade.com>
2023-06-06 12:17:53 +0300

    additional WIFI_FLAGs

--- tmos/services/WiFi/wifi_drv.h
+++ tmos/services/WiFi/wifi_drv.h
@@ -22,8 +22,10 @@
 
 // WIFI_FLAG_XXXX
 #define WIFI_FLAG_ON			0x01		//!< WiFi powered on
-#define WIFI_FLAG_REGISTERED	0x08		//!< WiFi registered in the network
-#define WIFI_FLAG_SHUTDOWN		0x80
+#define WIFI_FLAG_REGISTERED	0x02		//!< WiFi registered in the network
+#define WIFI_FLAG_DETECTED		0x04		//!< Supported WiFi module detected
+#define WIFI_FLAG_PRESENT		0x08		//!< physically connected WiFi but not recognized
+#define WIFI_FLAG_SHUTDOWN		0x80		//!< WiFi is in the process of being turned off or opening handlers is disabled
 
 // WIFI_STATUS_XXXX
 #define WIFI_STATE_OFF			0x00
@@ -42,13 +44,15 @@
 
 struct WIFI_DRIVER_DATA
 {
+	struct{
     unsigned char		wifi_clients;
     unsigned char		wifi_flags_ok;
     unsigned char		wifi_flags_bad;
 	DRIVER_INDEX		iface_driver_index;	//!< Index of the interface driver
+	}__attribute__ ((packed));
 	Task* 				wifi_task;
 
-    HANDLE				waiting;
+    HANDLE				wifi_waiting;
     wifi_module_type* 	wifi_module;		//!< Pointer to the detected wifi module (or NULL)
 
 	CHandle				helper_hnd;		//!< Handle used between the helper task and the driver
